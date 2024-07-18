b31060a124dfe459262581d7cea92d7ca3f256be
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-07-18 03:42:59 +0300

    Update ملصق منشن.js

--- plugins/ملصق منشن.js
+++ plugins/ملصق منشن.js
@@ -4,14 +4,14 @@ import fetch from 'node-fetch';
 let handler = m => m;
 
 handler.all = async function (m, conn) {
-    let img =["https://telegra.ph/file/57911e3e331dd31b4d565.png",
-              "https://telegra.ph/file/732b86b3159728d2a9c1f.png",
-             "https://telegra.ph/file/d9f64f2f785720606f720.png",
-             "https://telegra.ph/file/c42776b402bb9fca8ca04.png",
+    let img =["https://telegra.ph/file/ae5e5bfbc98cb7825f8ac.jpg",
+              "https://telegra.ph/file/ae5e5bfbc98cb7825f8ac.jpg",
+             "https://telegra.ph/file/ae5e5bfbc98cb7825f8ac.jpg",
+             "https://telegra.ph/file/ae5e5bfbc98cb7825f8ac.jpg",
              "https://telegra.ph/file/d870091e1b346afd2d30f.png",
              "https://telegra.ph/file/f7799a1459cac6eb1346c.png",
              "https://telegra.ph/file/4e84292a76a07ab824228.png",
-             "https://telegra.ph/file/0001a337cfcf778d4736c.png"];
+             "https://telegra.ph/file/ae5e5bfbc98cb7825f8ac.jpg"];
     let num = "201115618853";
     let img1 = await img[Math.floor(img.length * Math.random())];
     if (m.mentionedJid && m.mentionedJid[0]) {
