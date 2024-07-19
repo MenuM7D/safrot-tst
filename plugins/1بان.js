3778219e19cbe33c799e38bb7438c2e65fbf5fba
safrot1 <mostafam890u@gmail.com>
2024-07-20 00:16:09 +0300

    Update 1بان.js

--- plugins/1بان.js
+++ plugins/1بان.js
@@ -1,42 +1,47 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     let who
     if (m.isGroup) {
-        who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+        who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : null
     } else {
         who = m.chat
     }
     
-    if (!who) throw ` *منشن للي عايز تعملو بان*🧚🏼‍♂️`
+    if (!who) throw `*منشن للي عايز تعملو بان*🧚🏼‍♂️`
+
+    // Check if the user exists in the database
+   if (!(who in global.db.data.users)) throw `*المستخدم غير موجود في قاعدة البيانات*`
     
     let user = global.db.data.users[who]
     user.banned = true
     
-    let ban = 'https://qu.ax/SJJt.mp3'
+    let audioUrl = 'https://qu.ax/SJJt.mp3'
     let imageUrl = 'https://telegra.ph/file/5487258cdd4f40ee4d259.jpg'  // URL للصورة الثابتة
 
-    conn.sendMessage(m.chat, { 
-        audio: { url: ban }, 
+    await conn.sendMessage(m.chat, { 
+        caption: '*المستخدم خد بان مش هيعرف يستخدم اوامر البوت🧚🏼‍♂️*', 
         image: { url: imageUrl },  // إضافة الصورة هنا
         contextInfo: { 
-            "externalAdReply": { 
-                "title": `🧚🏼‍♂️ *مستخدم البان مش هيقدر يستخدم البوت* ${wm}`, 
-                "body": ``, 
-                "previewType": "PHOTO", 
-                "thumbnailUrl": null,
-                "thumbnail": imagen1, 
-                "sourceUrl": md, 
-                "showAdAttribution": true
+            externalAdReply: { 
+                title: `🧚🏼‍♂️ *تم البان*`,
+                body: `${wm}`, 
+                previewType: "PHOTO", 
+                thumbnail: { url: imageUrl },  // إضافة الصورة المصغرة هنا
+                sourceUrl: md, 
+                showAdAttribution: true
             }
-        }, 
-        ptt: true, 
-        mimetype: 'audio/mpeg', 
-        fileName: `error.mp3` 
+        }
     }, { quoted: m })
+
+    /*await conn.sendMessage(m.chat, { 
+        audio: { url: audioUrl },
+        mimetype: 'audio/mpeg',
+        ptt: true
+    }, { quoted: m })*/
 }
 
 handler.help = ['banuser']
 handler.tags = ['owner']
-handler.command = /^banuser$/i
+handler.command = /^banuser|بان$/i
 handler.rowner = true
 
 export default handler
