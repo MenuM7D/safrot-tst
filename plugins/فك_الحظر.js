b0e54e4c65f9f47f57018cdd32a0b8d2370ddec7
safrot13 <adamahmmed120@gmail.com>
2024-07-19 11:13:00 +0300

    Update and rename owner-unbanchat.js to فك_الحظر.js

---
+++ plugins/فك_الحظر.js
@@ -0,0 +1,25 @@
+let handler = async (m, { conn }) => {
+    if (!(m.chat in global.db.data.chats)) return m.reply('*الشات ده مش مسجل في قاعدة البيانات!*')
+    let chat = global.db.data.chats[m.chat]
+    if (!chat.isBanned) return m.reply('*الشات ده مش متحظر!!*')
+    chat.isBanned = false
+    conn.reply(m.chat,  '*البوت شغال 🚀*', m, {
+        contextInfo: { 
+            externalAdReply: { 
+                mediaUrl: null, 
+                mediaType: 1, 
+                description: null, 
+                title: ag, 
+                body: 'الشات اتفك عنه الحظر', 
+                previewType: 0, 
+                thumbnail: imagen4, 
+                sourceUrl: [md, yt, tiktok].getRandom() 
+            }
+        }
+    }) 
+}
+handler.command = /^فك_الحظر$/i
+//handler.botAdmin = true
+handler.rowner = true
+
+export default handler
