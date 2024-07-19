9d5f73fe7fddeac5acdfe57e9412bf55ab0c9fb9
safrot1 <mostafam890u@gmail.com>
2024-07-19 21:32:48 +0300

    Update and rename buyall.js to يشتري.js

--- plugins/يشتري.js
+++ plugins/يشتري.js
@@ -1,4 +1,3 @@
-
 const xppercredit = 350 
 let handler = async (m, { conn, command, args }) => {
   let count = command.replace(/^buy/i, '')
@@ -8,11 +7,11 @@ let handler = async (m, { conn, command, args }) => {
     global.db.data.users[m.sender].exp -= xppercredit * count
     global.db.data.users[m.sender].credit += count
     conn.reply(m.chat, `
-┌─「 *PAYMENT NOTE* 」
-‣ *Nominal purchase* : + ${count} 
-‣ *Spent* : -${xppercredit * count} XP
+┌─「 *إيصال الدفع* 」
+‣ *كمية الشراء* : + ${count} 
+‣ *المستنزف* : -${xppercredit * count} XP
 └──────────────`, m)
-  } else conn.reply(m.chat, `❎ Sorry, you don't have enough *XP* to buy *${count}* Gold\n\n You can get *XP* using the commands from the *games and economy menu*`, m)
+  } else conn.reply(m.chat, `❎ *آسف، ماعندكش كفاية XP عشان تشتري ${count} ذهب*\n\n* ممكن تجيب XP باستخدام الأوامر الموجودة في قائمة الألعاب والاقتصاد*`, m)
 }
 handler.help = ['buy', 'buyall']
 handler.tags = ['economy']
