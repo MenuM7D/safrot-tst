a77b27209ca37a82f58ed24a34b19a4c1c591fc6
safrot1 <mostafam890u@gmail.com>
2024-07-20 12:54:29 +0300

    Update and rename grupo-resetLink.js to رستر_الجروب.js

--- plugins/رستر_الجروب.js
+++ plugins/رستر_الجروب.js
@@ -1,10 +1,10 @@
 const handler = async (m, {conn}) => {
   const revoke = await conn.groupRevokeInvite(m.chat);
-  await conn.reply(m.chat, `*_Se restableció con éxito el link del grupo._*\n♾ • Link Nuevo: ${'https://chat.whatsapp.com/' + revoke}`, m);
+  await conn.reply(m.chat, `*_تم إعادة تعيين رابط المجموعة بنجاح._*\n♾ • الرابط الجديد: ${'https://chat.whatsapp.com/' + revoke}`, m);
 };
 handler.help = ['resetlink']
 handler.tags = ['group']
-handler.command = ['resetlink', 'revoke'];
+handler.command = ['رستر_الجروب', 'revoke'];
 handler.botAdmin = true;
 handler.admin = true;
 handler.group = true;
