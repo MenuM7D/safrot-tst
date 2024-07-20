3ac1ad53258975d7a7499a9075ae0ca06b2c3bd6
safrot1 <mostafam890u@gmail.com>
2024-07-20 11:13:28 +0300

    Update مستوايا.js

--- plugins/مستوايا.js
+++ plugins/مستوايا.js
@@ -46,7 +46,7 @@ let handler = async (m, { conn }) => {
 handler.help = ['levelup']
 handler.tags = ['econ']
 
-handler.command = ['مستوي', 'لفل', 'مستوى_أعلى', 'مستوايا'] 
+handler.command = ['مستوي', 'مستوى_أعلى', 'مستوايا'] 
 handler.register = true
 
 export default handler
