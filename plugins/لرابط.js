cabc13b7f5f2cda5b94325209a74f143836cb37f
safrot1 <mostafam890u@gmail.com>
2024-07-22 13:22:29 +0300

    Update and rename convertidor-tourl.js to لرابط.js

--- plugins/لرابط.js
+++ plugins/لرابط.js
@@ -1,16 +1,18 @@
 import uploadFile from '../lib/uploadFile.js';
 import uploadImage from '../lib/uploadImage.js';
+
 const handler = async (m) => {
   const q = m.quoted ? m.quoted : m;
   const mime = (q.msg || q).mimetype || '';
-  if (!mime) throw '*⚠️ ¿𝐘 𝐋𝐀 𝐈𝐌𝐀𝐆𝐄𝐍? 𝐑𝐞𝐬𝐩𝐨𝐧𝐝𝐞 𝐚 𝐮𝐧𝐚 𝐢𝐦𝐚𝐠𝐞𝐧 𝐨 𝐯𝐢𝐝𝐞𝐨 𝐞𝐥 𝐜𝐮𝐚𝐥 𝐬𝐞𝐫𝐚 𝐜𝐨𝐧𝐯𝐞𝐫𝐭𝐢𝐝𝐨 𝐚 𝐞𝐧𝐥𝐚𝐜𝐞*';
+  if (!mime) throw '*🧚🏼‍♂️ فين الصورة أو الفيديو؟ رد على صورة أو فيديو اللي عايز تحوله لرابط*';
   const media = await q.download();
   const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
   const link = await (isTele ? uploadImage : uploadFile)(media);
   m.reply(link);
 };
+
 handler.help = ['tourl <reply image>'];
 handler.tags = ['convertidor']
-handler.command = /^(upload|tourl)$/i;
+handler.command = /^(لرابط|tourl)$/i;
 handler.register = true
 export default handler;
