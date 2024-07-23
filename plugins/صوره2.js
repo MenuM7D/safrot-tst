36cf75f8e290fe6d7d0f505515ef90d3437554f0
safrot1 <mostafam890u@gmail.com>
2024-07-23 18:23:33 +0300

    Update صوره2.js

--- plugins/صوره2.js
+++ plugins/صوره2.js
@@ -1,7 +1,5 @@
 import { googleImage } from '@bochilteam/scraper'
 
-const canal2 = 'https://telegra.ph/file/078014e17aedf4f7a6cd9.jpg';
-
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `*[❗خطاء❗] مثال علي الامر ${usedPrefix + command} كانيكي*`
     if (m.text.includes('gore') || m.text.includes('cp') || m.text.includes('porno') || m.text.includes('Gore') || m.text.includes('rule') || m.text.includes('CP') || m.text.includes('Rule34')) 
@@ -17,25 +15,50 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         title: `عنوان القسم`,
         rows: [
             { header: 'عنوان1', title: "عنوان1", description: 'وصف1', id: usedPrefix + "menu" }, 
+            { header: 'عنوان2', title: "عنوان2", description: 'وصف2', id: usedPrefix + "command" },
         ],
     }]
 
     const messages = [[ 
-        'وصف الكروسل', 
-        'Footer الكروسل',
+        'Descripción de Carrusel 1', 
+        'Footer de Carrusel 1',
         'https://telegra.ph/file/5099b32eeb157d901030c.jpg',
-        [['زر1', usedPrefix + 'menu']],
-        [['نص للنسخ 1']],
-        [['رابط1', canal2]],
-        [['زر قائمة 1', sections]]
+        [['Botón1', usedPrefix + 'menu']],
+        [['Texto para copiar 1']],
+        [['Enlace1', 'https://example.com/link1']],
+        [['Botón Lista 1', sections]]
+    ], [
+        'Descripción de Carrusel 2',
+        'Footer de Carrusel 2',
+        'https://telegra.ph/file/e9239fa926d3a2ef48df2.jpg',
+        [['Botón2', usedPrefix + 'menu']],
+        [['Texto para copiar 2']],
+        [['Enlace2', 'https://example.com/link2']],
+        [['Botón Lista 2', sections]]
+    ], [
+        'Descripción de Carrusel 3',
+        'Footer de Carrusel 3',
+        'https://telegra.ph/file/7acad0975febb71446da5.jpg',
+        [['Botón3', usedPrefix + 'menu']],
+        [['Texto para copiar 3']],
+        [['Enlace3', 'https://example.com/link3']],
+        [['Botón Lista 3', sections]]
+    ], [
+        'Descripción de Carrusel 4',
+        'Footer de Carrusel 4',
+        'https://telegra.ph/file/24b24c495b5384b218b2f.jpg',
+        [['Botón4', usedPrefix + 'menu']],
+        [['Texto para copiar 4']],
+        [['Enlace4', 'https://example.com/link4']],
+        [['Botón Lista 4', sections]]
     ]]
 
-    await conn.sendCarousel(m.chat, 'نص', 'Footer', 'عنوان الكروسل', messages, m) 
+    await conn.sendCarousel(m.chat, 'Texto', 'Footer', 'Titulo de Carrusel', messages, m) 
 
-    conn.sendButton(m.chat, captionn, 'Author', link, [['صوره غيراها', `.صوره ${text}`]], m)
+    conn.sendButton(m.chat, captionn, 'Author', link, [['المنيو', `${usedPrefix}menu`]], m)
 }
 
 handler.help = ['gimage <query>', 'imagen <query>']
 handler.tags = ['internet', 'tools']
-handler.command = /^(gimage|image|صوره2|imagen)$/i
+handler.command = /^(gimage|image|2صوره|imagen)$/i
 export default handler
