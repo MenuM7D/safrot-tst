003fede380c8bbd63b678aa6ef1be920ce0e76c4
safrot1 <mostafam890u@gmail.com>
2024-07-23 03:35:06 +0300

    Update and rename descargas-playlist.js to -اغنيه.js

---
+++ plugins/-اغنيه.js
@@ -0,0 +1,46 @@
+import yts from 'yt-search';
+
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+  if (!text) return m.reply(`*هات اسم الاغنيه معا الامر يحب🧚🏼‍♂️*\n*• مثال*\n*${usedPrefix + command}* Bad Bunny`);
+  m.react('📀');
+
+  let result = await yts(text);
+  let ytres = result.videos;
+  let listSections = [];
+  for (let index in ytres) {
+    let v = ytres[index];
+    listSections.push({
+      title: `${index} | ${v.title}`,
+      rows: [{
+        header: '• • •「 🅐🅤🅓🅘🅞 」• • •',
+        title: "",
+        description: `▢ ⌚ مدة:* ${v.timestamp}\n▢ 👀 *مشاهدات:* ${v.views}\n▢ 📌 *تم النشر:* ${v.title}\n▢ 📆 *تم الرفع:* ${v.ago}\n`,
+        id: `${usedPrefix}fgmp3 ${v.url}`
+      }, {
+        header: "• • •「 🅥🅘🅓🅔🅞 」• • •",
+        title: "",
+        description: `▢ ⌚ مدة:* ${v.timestamp}\n▢ 👀 *مشاهدات:* ${v.views}\n▢ 📌 *تم النشر:* ${v.title}\n▢ 📆 *تم الرفع:* ${v.ago}\n`,
+        id: `${usedPrefix}fgmp4 ${v.url}`
+      }, {
+        header: "• • •「 🅓🅞🅒🅤🅜🅔🅝🅣🅞🅢 🅜🅟❸ 」• • •",
+        title: "",
+        description: `▢ ⌚ مدة:* ${v.timestamp}\n▢ 👀 *مشاهدات:* ${v.views}\n▢ 📌 *تم النشر:* ${v.title}\n▢ 📆 *تم الرفع:* ${v.ago}\n`,
+        id: `${usedPrefix}ytmp3doc ${v.url}`
+      }, {
+        header: "'• • •「 🅓🅞🅒🅤🅜🅔🅝🅣🅞🅢 🅜🅟❹ 」• • •",
+        title: "",
+        description: `▢ ⌚ مدة:* ${v.timestamp}\n▢ 👀 *مشاهدات:* ${v.views}\n▢ 📌 *تم النشر:* ${v.title}\n▢ 📆 *تم الرفع:* ${v.ago}\n`,
+        id: `${usedPrefix}ytmp4doc ${v.url}`
+      }]
+    });
+  }
+
+  await conn.sendList(m.chat, `*• النتائج لـ:* ${text}*\n\n> *اختار خيار واضغط إرسال*`, wm, `🚀 𝙍𝙀𝙎𝙐𝙇𝙏𝘼𝘿𝙊𝙎 🚀`, ytres[0].image, listSections, m);
+};
+
+handler.help = ['playlist', 'yts'];
+handler.tags = ['downloader'];
+handler.command = ['اغنيه', 'playlist', 'playlista', 'yts', 'ytsearch'];
+handler.register = true;
+
+export default handler;
