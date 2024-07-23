719da9421858a0f87e0a022242b096fe50368006
safrot1 <mostafam890u@gmail.com>
2024-07-23 03:58:16 +0300

    Update and rename convertidor-togifaud.js to -جيف.js

---
+++ plugins/-جيف.js
@@ -0,0 +1,18 @@
+/* 𝐂𝐑𝐄𝐀𝐃𝐎 𝐏𝐎𝐑 https://github.com/BrunoSobrino */
+
+const handler = async (m, {conn, usedPrefix, command}) => {
+  if (!m.quoted) throw `*🧚🏼‍♂️اعمل ريب ع الي عايز تحولو لجيف*`;
+  const q = m.quoted || m;
+  const mime = (q.msg || q).mimetype || '';
+  if (!/(mp4)/.test(mime)) throw `*🧚🏼‍♂️ نوع الملف ${mime} مش صحيح، رد على فيديو عشان نحوله لجيف بالصوت*`;
+  m.reply('*جارى التحميل🧚🏼‍♂️*');
+  const media = await q.download();
+  conn.sendMessage(m.chat, {video: media, gifPlayback: true, caption: '*اتفضل يحب🧚🏼‍♂️*'}, {quoted: m});
+};
+
+handler.help = ['togif'];
+handler.tags = ['محول'];
+handler.command = ['جيف', 'togif'];
+handler.register = true;
+
+export default handler;
