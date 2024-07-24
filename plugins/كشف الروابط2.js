0d022a81bf2e33ffab67a99ad7c1bd36b59125c1
safrot2 <safrotb20@gmail.com>
2024-07-24 04:05:00 +0300

    Update and rename _antilink2.js to كشف الروابط2.js

--- plugins/كشف الروابط2.js
+++ plugins/كشف الروابط2.js
@@ -18,13 +18,13 @@ if (m.text.includes(linkThisGroup)) return !0
 if (m.text.includes(linkThisGroup2)) return !0
 if (m.text.includes(linkThisGroup3)) return !0
 }    
-await conn.sendMessage(m.chat, {text: `*「 ANTILINK DETECTADO 」*\n\n${user} 🤨 Rompiste las reglas del Grupo sera eliminado....`, mentions: [m.sender]}, {quoted: m})
-if (!isBotAdmin) return m.reply('*Te salvarte gil, no soy admin no te puedo eliminar*')  
+await conn.sendMessage(m.chat, {text: `*「 كشف رابط 」*\n\n${user} 🤨 كسرت قواعد الجروب، هيتم طردك...`, mentions: [m.sender]}, {quoted: m})
+if (!isBotAdmin) return m.reply('*انت محظوظ، مش ادمن مش هقدر اطردك*')  
 if (isBotAdmin) {
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
 if (responseb[0].status === "404") return
-} else if (!bot.restrict) return m.reply('*𝙀𝙡 𝙥𝙧𝙤𝙥𝙞𝙚𝙩𝙖𝙧𝙞𝙤 𝙙𝙚𝙡 𝙗𝙤𝙩 𝙣𝙤 𝙩𝙞𝙚𝙣𝙚 𝙖𝙘𝙩𝙞𝙫𝙖𝙙𝙤 𝙚𝙡 𝙧𝙚𝙨𝙩𝙧𝙞𝙘𝙘𝙞𝙤́𝙣 (𝙚𝙣𝙖𝙗𝙡𝙚 𝙧𝙚𝙨𝙩𝙧𝙞𝙘𝙩) 𝙘𝙤𝙣𝙩𝙖𝙘𝙩𝙚 𝙘𝙤𝙣 𝙚𝙡 𝙥𝙖𝙧𝙖 𝙦𝙪𝙚 𝙡𝙤𝙨 𝙝𝙖𝙗𝙞𝙡𝙞𝙩𝙚*')
+} else if (!bot.restrict) return m.reply('*صاحب البوت مش مفعل خيار التقييد (enable restrict)، اتواصل معاه عشان يفعلها*')
 }
 return !0
-} 
+}
