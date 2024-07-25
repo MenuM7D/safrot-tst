38056059dfad8a254250bae36d0ad6c3ae5fb58f
safrot2 <safrotb20@gmail.com>
2024-07-26 01:59:27 +0300

    Update شغل-.js

--- plugins/شغل-.js
+++ plugins/شغل-.js
@@ -11,15 +11,18 @@ let apiUrlsz;
 let device;
 let dataMessage;
 let enviando = false;
+
 const handler = async (m, { command, usedPrefix, conn, text }) => {
-  const datas = global;
-  const idioma = datas.db.data.users[m.sender].language;
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`));
-  const tradutor = _translate.plugins.descargas_play_v2;
   device = await getDevice(m.key.id);
 
-  if (!text) throw `${tradutor.texto1[0]} _${usedPrefix + command} ${tradutor.texto1[1]} _${usedPrefix + command} https://youtu.be/JLWRZ8eWyZo?si=EmeS9fJvS_OkDk7p_`;
-  if (command === 'playyt' && (device == 'desktop' || device == 'web')) throw `*[❗] Los mensajes de botones aun no estan disponibles en WhatsApp web, acceda a su celular para poder ver y usar los mensajes con botones.*`;
+  if (!text) {
+    throw `يرجى توفير رابط أو عنوان فيديو YouTube. استخدم الأمر كما يلي: *${usedPrefix + command} <الرابط أو العنوان>*`;
+  }
+
+  if (command === 'playyt' && (device === 'desktop' || device === 'web')) {
+    throw `*[❗] لا تتوفر رسائل الأزرار في WhatsApp Web حالياً. يرجى استخدام هاتفك المحمول لمشاهدة واستخدام الرسائل مع الأزرار.*`;
+  }
+
   if (enviando) return;
   enviando = true;
 
@@ -30,11 +33,14 @@ const handler = async (m, { command, usedPrefix, conn, text }) => {
       `https://api-brunosobrino-dcaf9040.koyeb.app/api/ytplay?text=${text}`
     ];
     const linkyt = await isValidYouTubeLink(text);
-    if (linkyt) apiUrlsz = [
+    if (linkyt) {
+      apiUrlsz = [
         `https://api.cafirexos.com/api/ytinfo?url=${text}`,
         `https://api-brunosobrino-koiy.onrender.com/api/ytinfo?url=${text}&apikey=BrunoSobrino`,
         `https://api-brunosobrino-dcaf9040.koyeb.app/api/ytinfo?url=${text}`
-    ];
+      ];
+    }
+
     let success = false;
     for (const url of apiUrlsz) {
       try {
@@ -51,69 +57,72 @@ const handler = async (m, { command, usedPrefix, conn, text }) => {
       enviando = false;
       throw `_*< يوتيوب - تحميل />*_
 
-*[ ℹ️ ] ينقص عنوان فيديو YouTube.*
+*[ ℹ️ ] لم يتم العثور على عنوان فيديو YouTube.*
 
 *[ 💡 ] مثال:* _.شغل Good Feeling - Flo Rida_
 
 *[ 💡 ] مثال 2:* _.شغل https://youtu.be/JLWRZ8eWyZo?si=EmeS9fJvS_OkDk7p_`;
     }
 
-    const dataMessage = `العنوان : ${data.resultado.title}\nتم النشر : ${data.resultado.publicDate}\nالقناه : ${data.resultado.channel}\nرابط القناه : ${data.resultado.url}`.trim();  
-    if (!text.includes('SN@') && command !== 'شغل') await conn.sendMessage(m.chat, { text: dataMessage }, { quoted: m });      
-      
+    const dataMessage = `العنوان: ${data.resultado.title}\nتاريخ النشر: ${data.resultado.publicDate}\nالقناة: ${data.resultado.channel}\nرابط القناة: ${data.resultado.url}`.trim();
+
+    if (!text.includes('SN@') && command !== 'شغل') {
+      await conn.sendMessage(m.chat, { text: dataMessage }, { quoted: m });
+    }
+
     if (command === 'شغل') {
-      var messa = await prepareWAMessageMedia({ image: {url: data.resultado.image}}, { upload: conn.waUploadToServer });
+      var messa = await prepareWAMessageMedia({ image: { url: data.resultado.image } }, { upload: conn.waUploadToServer });
       let msg = generateWAMessageFromContent(m.chat, {
-          viewOnceMessage: {
-              message: {
-                  interactiveMessage: {
-                      body: { text: dataMessage },
-                      footer: { text: `©${global.wm}`.trim() },
-                      header: {
-                          hasMediaAttachment: true,
-                          imageMessage: messa.imageMessage,
-                      },
-                      nativeFlowMessage: {
-                          buttons: [
-                              {
-                                  name: 'quick_reply',
-                                  buttonParamsJson: JSON.stringify({
-                                      display_text: 'صـوت 🎧',
-                                      id: `${usedPrefix}play.1 ${data.resultado.url} SN@`
-                                  })
-                              },
-                              {
-                                  name: 'quick_reply',
-                                  buttonParamsJson: JSON.stringify({
-                                      display_text: 'ملـف صـوت 📼',
-                                      id: `${usedPrefix}صوتيات ${data.resultado.url} SN@`
-                                  })
-                              },
-                             {
-                                  name: 'quick_reply',
-                                  buttonParamsJson: JSON.stringify({
-                                      display_text: 'فيـديو 🎥',
-                                      id: `${usedPrefix}play.2 ${data.resultado.url} SN@`
-                                  })
-                              },
-                             {
-                                  name: 'quick_reply',
-                                  buttonParamsJson: JSON.stringify({
-                                      display_text: 'ملـف فيـديو 🎬',
-                                      id: `${usedPrefix}فيديوهات ${data.resultado.url} SN@`
-                                  })
-                              },   
-                          ],
-                          messageParamsJson: "",
-                      },
-                  },
+        viewOnceMessage: {
+          message: {
+            interactiveMessage: {
+              body: { text: dataMessage },
+              footer: { text: `©${global.wm}`.trim() },
+              header: {
+                hasMediaAttachment: true,
+                imageMessage: messa.imageMessage,
               },
-          }
-      }, { userJid: conn.user.jid, quoted: m});
-      await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id});
-      enviando = false;    
+              nativeFlowMessage: {
+                buttons: [
+                  {
+                    name: 'quick_reply',
+                    buttonParamsJson: JSON.stringify({
+                      display_text: 'صوت 🎧',
+                      id: `${usedPrefix}play.1 ${data.resultado.url} SN@`
+                    })
+                  },
+                  {
+                    name: 'quick_reply',
+                    buttonParamsJson: JSON.stringify({
+                      display_text: 'ملف صوت 📼',
+                      id: `${usedPrefix}صوتيات ${data.resultado.url} SN@`
+                    })
+                  },
+                  {
+                    name: 'quick_reply',
+                    buttonParamsJson: JSON.stringify({
+                      display_text: 'فيديو 🎥',
+                      id: `${usedPrefix}play.2 ${data.resultado.url} SN@`
+                    })
+                  },
+                  {
+                    name: 'quick_reply',
+                    buttonParamsJson: JSON.stringify({
+                      display_text: 'ملف فيديو 🎬',
+                      id: `${usedPrefix}فيديوهات ${data.resultado.url} SN@`
+                    })
+                  },
+                ],
+                messageParamsJson: "",
+              },
+            },
+          },
+        }
+      }, { userJid: conn.user.jid, quoted: m });
+      await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id });
+      enviando = false;
       return;
-    }    
+    }
 
     try {
       if (command === 'play.1') {
@@ -140,12 +149,12 @@ const handler = async (m, { command, usedPrefix, conn, text }) => {
 
         if (!success2) {
           enviando = false;
-          throw `تم بنجاه ٣`;
+          throw `فشل تحميل الملف الصوتي. حاول مرة أخرى لاحقاً.`;
         }
       } else if (command === 'play.2') {
         let apiUrls22 = [
           `https://api.cafirexos.com/api/v1/ytmp4?url=${data.resultado.url}`,
-          `https://api.cafirexos.com/api/v2/ytmp4?url=${data.resultado.url}`,            
+          `https://api.cafirexos.com/api/v2/ytmp4?url=${data.resultado.url}`,
           `https://api-brunosobrino.onrender.com/api/v1/ytmp4?url=${data.resultado.url}&apikey=BrunoSobrino`,
           `https://api-brunosobrino.onrender.com/api/v2/ytmp4?url=${data.resultado.url}&apikey=BrunoSobrino`,
           `https://api-brunosobrino-dcaf9040.koyeb.app/api/v1/ytmp4?url=${data.resultado.url}`,
@@ -162,32 +171,32 @@ const handler = async (m, { command, usedPrefix, conn, text }) => {
             success2 = true;
             break;
           } catch (e) {
-             console.log(e.message) 
+            console.log(e.message);
           }
         }
 
         if (!success2) {
           enviando = false;
-          throw `تم بنجاح ٢`;
+          throw `فشل تحميل الملف الفيديو. حاول مرة أخرى لاحقاً.`;
         }
       }
     } catch (ee) {
-      console.log(ee.message)  
+      console.log(ee.message);
       enviando = false;
-      throw `لوج`;
+      throw `حدث خطأ أثناء تحميل الملف.`;
     }
 
     if (buff) {
-      await conn.sendMessage(m.chat, {[mimeType.startsWith('audio') ? 'audio' : 'video']: buff.data, mimetype: mimeType, fileName: fileName}, {quoted: m});
+      await conn.sendMessage(m.chat, { [mimeType.startsWith('audio') ? 'audio' : 'video']: buff.data, mimetype: mimeType, fileName: fileName }, { quoted: m });
       enviando = false;
     } else {
       enviando = false;
-      throw `ت٥`;
+      throw `حدث خطأ.`;
     }
   } catch (error) {
-    console.log(error);  
+    console.log(error);
     enviando = false;
-    throw اريرور;
+    throw `خطأ: ${error}`;
   }
 };
 
@@ -195,7 +204,16 @@ handler.command = /^(play.1|play.2|شغل)$/i;
 export default handler;
 
 async function isValidYouTubeLink(link) {
-    const validPatterns = [/youtube\.com\/watch\?v=/i, /youtube\.com\/shorts\//i, /youtu\.be\//i, /youtube\.com\/embed\//i, /youtube\.com\/v\//i, /youtube\.com\/attribution_link\?a=/i, /yt\.be\//i, /googlevideo\.com\//i, /youtube\.com\.br\//i, /youtube-nocookie\.com\//i, /youtubeeducation\.com\//i, /m\.youtube\.com\//i, /youtubei\.googleapis\.com\//i];
-    return validPatterns.some(pattern => pattern.test(link));
-  }
-      
+  const validPatterns = [
+    /youtube\.com\/watch\?v=/i,
+    /youtube\.com\/shorts\//i,
+    /youtu\.be\//i,
+    /youtube\.com\/embed\//i,
+    /youtube\.com\/v\//i,
+    /youtube\.com\/attribution_link\?a=/i,
+    /yt\.be\//i,
+    /googlevideo\.com\//i,
+    /youtube\.com\.br\//i,
+    /youtube-nocookie\.com\//i,
+    /youtubeeducation\.com\//i,
+    /m\.youtube
