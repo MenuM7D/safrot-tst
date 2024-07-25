3ba4d1e0163daec45993df7f1ab7471242ee2f44
safrot2 <safrotb20@gmail.com>
2024-07-25 23:37:37 +0300

    Update معلوم_الجروب.js

--- plugins/معلوم_الجروب.js
+++ plugins/معلوم_الجروب.js
@@ -4,13 +4,13 @@ let handler = async (m, { conn, participants, groupMetadata }) => {
     const groupAdmins = participants.filter(p => p.admin);
     const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n');
     const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
-    let text = `『 معلومات المجموعة 』\n\n*• \`『 الرقم المعرف 』\` *\n${groupMetadata.id}\n\n*• \`『 الاسم 』\` *\n${groupMetadata.subject}\n\n*• \`『 عدد الاعضاء 』\` *\n${participants.length}\n\n*• مالك المجموعة :*\n@${owner.split('@')[0]}\n\n*• المشرفين :*\n${listAdmin}\n\n*• إعدادات المجموعة :*\n• الترحيب: ${welcome ? '✅' : '❌'}\n• الحظر: ${isBanned ? '✅' : '❌'}\n• الكشف: ${detect ? '✅' : '❌'}\n• منع الحذف: ${del ? '✅' : '❌'}\n• منع الروابط: ${antiLink ? '✅' : '❌'}\n\n*• إعدادات الردود :*\n• الترحيب: ${sWelcome}\n• الوداع: ${sBye}\n• الترقية: ${sPromote}\n• التخفيض: ${sDemote}\n\n*• الوصف :*\n${groupMetadata.desc?.toString() || 'غير معروف'}`.trim();
+    let text = `『 معلومات المجموعة 』\n\n*• \`『 الرقم المعرف 』\` *\n${groupMetadata.id}\n\n*• \`『 الاسم 』\` *\n${groupMetadata.subject}\n\n*• \`『 عدد الاعضاء 』\` *\n${participants.length}\n\n*• \`『 مالك الجروب 』\` *\n@${owner.split('@')[0]}\n\n*• \`『 المشرفين 』\` *\n${listAdmin}\n\n*• \`『 اعددات الجروب 』\` :*\n• \`『 الترحيب 』\`: ${welcome ? '✅' : '❌'}\n• الحظر: ${isBanned ? '✅' : '❌'}\n• الكشف: ${detect ? '✅' : '❌'}\n• منع الحذف: ${del ? '✅' : '❌'}\n• منع الروابط: ${antiLink ? '✅' : '❌'}\n\n*• إعدادات الردود :*\n• الترحيب: ${sWelcome}\n• الوداع: ${sBye}\n• الترقية: ${sPromote}\n• التخفيض: ${sDemote}\n\n*• الوصف :*\n${groupMetadata.desc?.toString() || 'غير معروف'}`.trim();
     conn.sendFile(m.chat, pp, 'pp.jpg', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] });
 }
 
 handler.help = ['معلومات_المجموعة'];
 handler.tags = ['group'];
-handler.command = ['معلومات_مجموعة', '.معلوم_الجروب'];
+handler.command = ['معلومات_الجروب', 'معلوم_الجروب'];
 handler.group = true;
 //handler.register = true;
 
