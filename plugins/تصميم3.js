2e8a73f7e6f1f079bf28b179c477b19f9041e1e2
safrot2 <safrotb20@gmail.com>
2024-07-26 03:32:13 +0300

    Update تصميم3.js

--- plugins/تصميم3.js
+++ plugins/تصميم3.js
@@ -12,23 +12,50 @@ let handler = async (m, {
     text,
     command
 }) => {
-    if (!text) throw "هذا الامر خاص بعمل شعارات باسمك \nمثال:\n*.تصميم3* safrot bot"
+    if (!text) throw "الأمر ده لعمل شعارات باسمك \nمثال:\n*.تصميم3* safrot bot"
     try {
-            await m.reply(wait)
-            let res = await BrandCrowd(text)
-            let rdm = res[Math.floor(Math.random() * res.length)];
-            await conn.sendMessage(m.chat, {
-                image: {
-                    url: rdm
-                }, caption: "[ النتيجة ]"
-            }, {
-                quoted: m
-            })
+        await m.reply("استنى شوية...")
+        let res = await BrandCrowd(text)
+        let rdm = res[Math.floor(Math.random() * res.length)];
+        await conn.sendMessage(m.chat, {
+            image: {
+                url: rdm
+            }, caption: "[ النتيجة ]"
+        }, {
+            quoted: m
+        })
+
+        // زرار لتغيير الصورة
+        await conn.sendMessage(m.chat, {
+            text: "عايز تغير الصورة؟ اضغط الزر ده",
+            buttons: [
+                { buttonId: 'change_logo', buttonText: { displayText: 'تغيير الصورة' }, type: 1 }
+            ],
+            headerType: 1
+        }, {
+            quoted: m
+        })
 
     } catch (e) {
-        throw eror
+        throw e
     }
 }
+
+// التعامل مع زر تغيير الصورة
+conn.on('button_click', async (button) => {
+    if (button.buttonId === 'change_logo') {
+        let res = await BrandCrowd(text)
+        let rdm = res[Math.floor(Math.random() * res.length)];
+        await conn.sendMessage(button.chat, {
+            image: {
+                url: rdm
+            }, caption: "[ النتيجة الجديدة ]"
+        }, {
+            quoted: button.message
+        })
+    }
+})
+
 handler.help = ["brandcrowd"]
 handler.tags = ["logo"]
 handler.command = /^تصميم3$/i
@@ -37,16 +64,16 @@ export default handler
 
 /* New Line */
 async function BrandCrowd(query) {
-let res = await fetch('https://www.brandcrowd.com/maker/logos/page1?Text=' +query+ '&TextChanged=true&SearchText&KeywordChanged=true&LogoStyle=0&FontStyles&Colors&FilterByTags')
+    let res = await fetch('https://www.brandcrowd.com/maker/logos/page1?Text=' + query + '&TextChanged=true&SearchText&KeywordChanged=true&LogoStyle=0&FontStyles&Colors&FilterByTags')
     let html = await res.text()
     let dom = new JSDOM(html)
     var collection = dom.window.document.getElementsByTagName('img');
     let img = []
-for (var i = 0; i < collection.length; i++) {
-	if (collection[i].getAttribute('src').startsWith('https://dynamic.brandcrowd.com')) {
-	img.push(collection[i].getAttribute('src'))
-	}
-}
-let newArr = img.filter(el => el != null);
-return newArr
-}
+    for (var i = 0; i < collection.length; i++) {
+        if (collection[i].getAttribute('src').startsWith('https://dynamic.brandcrowd.com')) {
+            img.push(collection[i].getAttribute('src'))
+        }
+    }
+    let newArr = img.filter(el => el != null);
+    return newArr
+    }
