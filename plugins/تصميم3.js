ee3c32a7ce1e20f6c3d55dd7bb50548f173db8d3
safrot2 <safrotb20@gmail.com>
2024-07-26 03:39:35 +0300

    Update تصميم3.js

--- plugins/تصميم3.js
+++ plugins/تصميم3.js
@@ -1,29 +1,18 @@
 import axios from "axios"
 import fetch from "node-fetch"
 import cheerio from "cheerio"
-import {
-    JSDOM
-} from "jsdom"
+import { JSDOM } from "jsdom"
 
-let handler = async (m, {
-    conn,
-    args,
-    usedPrefix,
-    text,
-    command
-}) => {
+let handler = async (m, { conn, args, usedPrefix, text, command }) => {
     if (!text) throw "الأمر ده لعمل شعارات باسمك \nمثال:\n*.تصميم3* safrot bot"
     try {
         await m.reply("استنى شوية...")
         let res = await BrandCrowd(text)
         let rdm = res[Math.floor(Math.random() * res.length)];
         await conn.sendMessage(m.chat, {
-            image: {
-                url: rdm
-            }, caption: "[ النتيجة ]"
-        }, {
-            quoted: m
-        })
+            image: { url: rdm },
+            caption: "[ النتيجة ]"
+        }, { quoted: m })
 
         // زرار لتغيير الصورة
         await conn.sendMessage(m.chat, {
@@ -32,9 +21,10 @@ let handler = async (m, {
                 { buttonId: 'change_logo', buttonText: { displayText: 'تغيير الصورة' }, type: 1 }
             ],
             headerType: 1
-        }, {
-            quoted: m
-        })
+        }, { quoted: m })
+        
+        // تخزين الشعار الناتج
+        m.logoResults = res;
 
     } catch (e) {
         throw e
@@ -43,16 +33,13 @@ let handler = async (m, {
 
 // التعامل مع زر تغيير الصورة
 conn.on('button_click', async (button) => {
-    if (button.buttonId === 'change_logo') {
-        let res = await BrandCrowd(text)
+    if (button.buttonId === 'change_logo' && button.message && button.message.logoResults) {
+        let res = button.message.logoResults;
         let rdm = res[Math.floor(Math.random() * res.length)];
         await conn.sendMessage(button.chat, {
-            image: {
-                url: rdm
-            }, caption: "[ النتيجة الجديدة ]"
-        }, {
-            quoted: button.message
-        })
+            image: { url: rdm },
+            caption: "[ النتيجة الجديدة ]"
+        }, { quoted: button.message })
     }
 })
 
@@ -67,13 +54,12 @@ async function BrandCrowd(query) {
     let res = await fetch('https://www.brandcrowd.com/maker/logos/page1?Text=' + query + '&TextChanged=true&SearchText&KeywordChanged=true&LogoStyle=0&FontStyles&Colors&FilterByTags')
     let html = await res.text()
     let dom = new JSDOM(html)
-    var collection = dom.window.document.getElementsByTagName('img');
+    let collection = dom.window.document.getElementsByTagName('img');
     let img = []
-    for (var i = 0; i < collection.length; i++) {
+    for (let i = 0; i < collection.length; i++) {
         if (collection[i].getAttribute('src').startsWith('https://dynamic.brandcrowd.com')) {
             img.push(collection[i].getAttribute('src'))
         }
     }
-    let newArr = img.filter(el => el != null);
-    return newArr
-    }
+    return img.filter(el => el != null);
+            }
