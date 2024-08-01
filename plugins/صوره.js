a27247c89718e8cdb5daa482467008e6eb31f1ec
safrot3 <safrot031@gmail.com>
2024-08-01 19:36:03 +0300

    Update صوره.js

--- plugins/صوره.js
+++ plugins/صوره.js
@@ -1,22 +1,39 @@
-import {googleImage} from '@bochilteam/scraper';
+import { googleImage } from '@bochilteam/scraper';
 
-const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) return conn.reply(m.chat, `*اكتب اسم الصوره الي بدور عليها🧚🏼‍♂️*\n•  مثال\n*${usedPrefix + command} خلفيه*`, m, {contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: iig, body: ' 𝙎𝙖𝙛𝙧𝙤𝙩-𝙈𝘿 ', previewType: 0, thumbnail: img.getRandom(), sourceUrl: redes.getRandom()}}});
-  
-  if (m.text.includes('gore') || m.text.includes('cp') || m.text.includes('porno') || m.text.includes('Gore') || m.text.includes('rule') || m.text.includes('CP') || m.text.includes('Rule34') || m.text.includes('xxx','سكس','نودز','شرموطه','كس','نيك','سكسي','عهيرات','شراميط','لبوه','اكساس','xnxx')) 
-    throw 'طلبك مرفود يكسمك🙄';
-  
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) {
+    return conn.reply(m.chat, `*اكتب اسم الصوره الي بدور عليها🧚🏼‍♂️*\n• مثال\n*${usedPrefix + command} خلفيه*`, m, {
+      contextInfo: {
+        externalAdReply: {
+          mediaUrl: null,
+          mediaType: 1,
+          description: null,
+          title: 'بحث صور', // تم تعيين عنوان افتراضي
+          body: ' 𝙎𝙖𝙛𝙧𝙤𝙩-𝙈𝘿 ',
+          previewType: 0,
+          thumbnail: null, // يمكنك تعيين صورة افتراضية هنا
+          sourceUrl: null // يمكنك تعيين رابط افتراضي هنا
+        }
+      }
+    });
+  }
+
+  const blockedWords = ['gore', 'cp', 'porno', 'rule34', 'xxx', 'سكس', 'نودز', 'شرموطه', 'كس', 'نيك', 'سكسي', 'عهيرات', 'شراميط', 'لبوه', 'اكساس', 'xnxx'];
+  if (blockedWords.some(word => m.text.toLowerCase().includes(word))) {
+    throw 'طلبك مرفوض يكسمك🙄';
+  }
+
   const res = await googleImage(text);
   const image = await res.getRandom();
   const link = image;
-  
-  conn.sendButton(m.chat, `🧚🏼‍♂️ *البحث*: ${text}`, botname, link, [['🧚🏼‍♂️ صوره تاني 🧚🏼‍♂️', `/${command} ${text}`]], null, null, m, null, fake);
+
+  conn.sendButton(m.chat, `🧚🏼‍♂️ *البحث*: ${text}`, 'اسم البوت', link, [['🧚🏼‍♂️ صوره تاني 🧚🏼‍♂️', `/${command} ${text}`]], m);
 };
 
-handler.help = ['صوره2'];
+handler.help = ['صوره'];
 handler.tags = ['buscadores'];
 handler.command = /^(gimage|صوره|صورة|imagen)$/i;
-//handler.register = true;
+// handler.register = true;
 handler.limit = 1;
 
 export default handler;
