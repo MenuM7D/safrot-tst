41e53861c7b3f1a94a880543d44d687babd3c96e
safrot3 <safrot031@gmail.com>
2024-08-01 16:36:25 +0300

    Update الغاء_الحظر.js

--- plugins/الغاء_الحظر.js
+++ plugins/الغاء_الحظر.js
@@ -1,6 +1,6 @@
 let handler = async (m) => {
 global.db.data.chats[m.chat].isBanned = false
-m.reply('*\`『 تم الغاء الحظر 🧚🏼‍♂️ 』\`*')
+m.reply(' *\`『 تم الغاء الحظر 🧚🏼‍♂️ 』\`* ')
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
