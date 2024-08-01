10a2272d5751b089f46788109d6140692eddb7d7
safrot3 <safrot031@gmail.com>
2024-08-01 16:28:08 +0300

    Update and rename الغاء-الحظر.js to الغاء_الحظر.js

--- plugins/الغاء_الحظر.js
+++ plugins/الغاء_الحظر.js
@@ -1,9 +1,9 @@
 let handler = async (m) => {
 global.db.data.chats[m.chat].isBanned = false
-m.reply('* تم إلغاء حظر هذه الدردشة بنجاح*')
+m.reply('*\`『 تم الغاء الحظر 🧚🏼‍♂️ 』\`*')
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
-handler.command = /^الغاء-الحظر$/i
+handler.command = /^الغاء_الحظر$/i
 //handler.rowner = true
 export default handler
