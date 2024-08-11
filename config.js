0a1d943a4eaf437bc907af98f467a1af3eec5b3b
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-08-11 14:00:54 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,96 +1,96 @@
-import { watchFile, unwatchFile } from 'fs' 
-import chalk from 'chalk'
-import { fileURLToPath } from 'url'
-import fs from 'fs'
-const cheerio = require('cheerio');
-import fetch from 'node-fetch'
-import axios from 'axios'
-import moment from 'moment-timezone' 
+import { watchFile, unwatchFile } from 'fs';
+import chalk from 'chalk';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import cheerio from 'cheerio';
+import fetch from 'node-fetch';
+import axios from 'axios';
+import moment from 'moment-timezone';
 
 //---------[ Añada los numeros a ser Propietario/a ]---------
-
-global.owner = [['201115618853', '♯ЅᗩFᏒOT꙯', true], ['201115618853'], ['201115618853'], ['201115618853'], ['201115618853'], ['201115618853'], ['201115618853'], ['201115618853']]
-global.mods = []
-global.prems = []
+global.owner = [['201115618853', '♯ЅᗩFᏒOT꙯', true], ['201115618853'], ['201115618853'], ['201115618853'], ['201115618853'], ['201115618853'], ['201115618853'], ['201115618853']];
+global.mods = [];
+global.prems = [];
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumberCode = "" //Ejemplo: +59309090909
-global.confirmCode = "" 
+global.botNumberCode = ""; //Ejemplo: +59309090909
+global.confirmCode = "";
 
 //---------[ APIS GLOBAL ]---------
+global.openai_key = 'sk-...OzYy'; /* Consigue tu ApiKey en este enlace: https://platform.openai.com/account/api-keys */
+global.openai_org_id = 'HITjoN7H8pCwoncEB9e3fSyW'; /* Consigue tu ID de organizacion en este enlace: https://platform.openai.com/account/org-settings */
+global.Key360 = ['964f-0c75-7afc']; //key de violetics
+global.keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f'];
+global.keysxxx = global.keysZens[Math.floor(global.keysZens.length * Math.random())];
+global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63'];
+global.keysxteam = global.keysxteammm[Math.floor(global.keysxteammm.length * Math.random())];
+global.keysneoxrrr = ['5VC9rvNx', 'cfALv5'];
+global.keysneoxr = global.keysneoxrrr[Math.floor(global.keysneoxrrr.length * Math.random())];
+global.lolkeysapi = "GataDiosV2";
+global.itsrose = ['4b146102c4d500809da9d1ff'];
+global.baileys = '@whiskeysockets/baileys';
 
-global.openai_key = 'sk-...OzYy' /* Consigue tu ApiKey en este enlace: https://platform.openai.com/account/api-keys */
-global.openai_org_id = 'HITjoN7H8pCwoncEB9e3fSyW' /* Consigue tu ID de organizacion en este enlace: https://platform.openai.com/account/org-settings */
-global.Key360 = ['964f-0c75-7afc']//key de violetics
-global.keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f']
-global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
-global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
-global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
-global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
-global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
-global.lolkeysapi = "GataDiosV2"
-global.itsrose = ['4b146102c4d500809da9d1ff']
-global.baileys = '@whiskeysockets/baileys'
+global.APIs = {
+  xteam: 'https://api.xteam.xyz',
+  dzx: 'https://api.dhamzxploit.my.id',
+  lol: 'https://api.lolhuman.xyz',
+  violetics: 'https://violetics.pw',
+  neoxr: 'https://api.neoxr.my.id',
+  zenzapis: 'https://api.zahwazein.xyz',
+  akuari: 'https://api.akuari.my.id',
+  akuari2: 'https://apimu.my.id',
+  fgmods: 'https://api-fgmods.ddns.net',
+  botcahx: 'https://api.botcahx.biz.id',
+  ibeng: 'https://api.ibeng.tech/docs',
+  rose: 'https://api.itsrose.site',
+  popcat: 'https://api.popcat.xyz',
+  xcoders: 'https://api-xcoders.site'
+};
+global.APIKeys = {
+  'https://api.xteam.xyz': `${global.keysxteam}`,
+  'https://api.lolhuman.xyz': `${global.lolkeysapi}`,
+  'https://api.neoxr.my.id': `${global.keysneoxr}`,
+  'https://violetics.pw': 'beta',
+  'https://api.zahwazein.xyz': `${global.keysxxx}`,
+  'https://api-fgmods.ddns.net': 'fg-dylux',
+  'https://api.botcahx.biz.id': 'Admin',
+  'https://api.ibeng.tech/docs': 'tamvan',
+  'https://api.itsrose.site': 'Rs-Zeltoria',
+  'https://api-xcoders.site': 'Frieren'
+};
 
-global.APIs = {xteam: 'https://api.xteam.xyz', 
-dzx: 'https://api.dhamzxploit.my.id',
-lol: 'https://api.lolhuman.xyz',
-violetics: 'https://violetics.pw',
-neoxr: 'https://api.neoxr.my.id',
-zenzapis: 'https://api.zahwazein.xyz',
-akuari: 'https://api.akuari.my.id',
-akuari2: 'https://apimu.my.id',        
-fgmods: 'https://api-fgmods.ddns.net',
-botcahx: 'https://api.botcahx.biz.id',
-ibeng: 'https://api.ibeng.tech/docs',        
-rose: 'https://api.itsrose.site',
-popcat : 'https://api.popcat.xyz',
-xcoders : 'https://api-xcoders.site' },
-global.APIKeys = {'https://api.xteam.xyz': `${keysxteam}`,
-'https://api.lolhuman.xyz': `${lolkeysapi}`,
-'https://api.neoxr.my.id': `${keysneoxr}`,        
-'https://violetics.pw': 'beta',
-'https://api.zahwazein.xyz': `${keysxxx}`,
-'https://api-fgmods.ddns.net': 'fg-dylux',
-'https://api.botcahx.biz.id': 'Admin',
-'https://api.ibeng.tech/docs': 'tamvan',
-'https://api.itsrose.site': 'Rs-Zeltoria',
-'https://api-xcoders.site': 'Frieren' }
-
-global.cheerio = cheerio
-global.fs = fs
-global.fetch = fetch
-global.axios = axios
-global.moment = moment        
+global.cheerio = cheerio;
+global.fs = fs;
+global.fetch = fetch;
+global.axios = axios;
+global.moment = moment;
 
 //------------------------[ Stickers ]-----------------------------
-
-global.packname = 'ₛₐfᵣₒₜ↯bₒₜ'
-global.author = '𝙎𝙖𝙛𝙧𝙤𝙩-𝙈𝘿'
+global.packname = 'ₛₐfᵣₒₜ↯bₒₜ';
+global.author = '𝙎𝙖𝙛𝙧𝙤𝙩-𝙈𝘿';
 
 //------------[ Versión | Nombre | cuentas ]------------
+global.wm = '𝙎𝙖𝙛𝙧𝙤𝙩-𝙈𝘿';
+global.botname = '𝙎𝙖𝙛𝙧𝙤𝙩-𝙈𝘿';
+global.vs = '1.9.5';
+global.yt = 'https://youtube.com/@safrot?si=RATPR9wHWaEmw8hi';
+global.tiktok = 'tiktok.com/@safrot_1';
+global.md = 'https://www.atom.bio/safrotbob-376';
+global.fb = 'https://www.facebook.com/safrotbob?mibextid=ZbWKwL';
+global.face = 'https://www.facebook.com/safrotbob?mibextid=ZbWKwL';
 
-global.wm = '𝙎𝙖𝙛𝙧𝙤𝙩-𝙈𝘿' 
-global.botname = '𝙎𝙖𝙛𝙧𝙤𝙩-𝙈𝘿'
-global.vs = '1.9.5'
-global.yt = 'https://youtube.com/@safrot?si=RATPR9wHWaEmw8hi'
-global.tiktok = 'tiktok.com/@safrot_1'
-global.md = 'https://www.atom.bio/safrotbob-376'
-global.fb = 'https://www.facebook.com/safrotbob?mibextid=ZbWKwL'
-global.face = 'https://www.facebook.com/safrotbob?mibextid=ZbWKwL'
-
-global.nna = 'https://whatsapp.com/channel/0029VaeXAKJAjPXLKGuZSr46' //Update
-global.nn = 'https://chat.whatsapp.com/F7fejXxmBAIKyrMVpYJExK' //Grupo 1
-global.nnn = 'https://chat.whatsapp.com/F7fejXxmBAIKyrMVpYJExK' //Grupo 2
-global.nnnt = 'https://chat.whatsapp.com/F7fejXxmBAIKyrMVpYJExK' //Grupo del Colaboracion
-global.nnnt2 = 'https://chat.whatsapp.com/F7fejXxmBAIKyrMVpYJExK' // Grupo COL 2
-global.nnntt = 'https://chat.whatsapp.com/F7fejXxmBAIKyrMVpYJExK' //Grupo COL 3
-global.nnnttt = 'https://chat.whatsapp.com/F7fejXxmBAIKyrMVpYJExK' //enlace lolibot
-global.nnntttt = 'https://chat.whatsapp.com/F7fejXxmBAIKyrMVpYJExK' //Grupo ayuda sobre el bot
-global.bot = 'Wa.me/201115618853'
-global.asistencia = `${fb}`
-global.redes = [nna, yt, nn, md, tiktok, fb, nnn, face]
+global.nna = 'https://whatsapp.com/channel/0029VaeXAKJAjPXLKGuZSr46'; //Update
+global.nn = 'https://chat.whatsapp.com/F7fejXxmBAIKyrMVpYJExK'; //Grupo 1
+global.nnn = 'https://chat.whatsapp.com/F7fejXxmBAIKyrMVpYJExK'; //Grupo 2
+global.nnnt = 'https://chat.whatsapp.com/F7fejXxmBAIKyrMVpYJExK'; //Grupo del Colaboracion
+global.nnnt2 = 'https://chat.whatsapp.com/F7fejXxmBAIKyrMVpYJExK'; // Grupo COL 2
+global.nnntt = 'https://chat.whatsapp.com/F7fejXxmBAIKyrMVpYJExK'; //Grupo COL 3
+global.nnnttt = 'https://chat.whatsapp.com/F7fejXxmBAIKyrMVpYJExK'; //enlace lolibot
+global.nnntttt = 'https://chat.whatsapp.com/F7fejXxmBAIKyrMVpYJExK'; //Grupo ayuda sobre el bot
+global.bot = 'Wa.me/201115618853';
+global.asistencia = `${global.fb}`;
+global.redes = [global.nna, global.yt, global.nn, global.md, global.tiktok, global.fb, global.nnn, global.face];
 
 //------------------------[ Info | Datos ]---------------------------
 
