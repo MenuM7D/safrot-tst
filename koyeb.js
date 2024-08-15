832b104ca1f22589a05741f72af9b361cc4fd732
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-08-15 18:10:18 +0300

    Update koyeb.js

--- koyeb.js
+++ koyeb.js
@@ -1,16 +1,23 @@
-//FROM fedora:37
- 
-RUN sudo dnf -y update &&\
-    sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &&\
-    sudo dnf install -y git ffmpeg ImageMagick nodejs yarnpkg libwebp &&\
-    sudo dnf clean all -y
+# استخدام فيدورا 37 كصورة أساسية
+FROM fedora:37
 
+# تحديث النظام وتثبيت الحزم المطلوبة
+RUN dnf -y update &&\
+    dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &&\
+    dnf install -y git ffmpeg ImageMagick nodejs yarnpkg libwebp &&\
+    dnf clean all
+
+# استنساخ مستودع GitHub
 RUN git clone https://github.com/elrebelde21/The-LoliBot-MD
 
+# تغيير مسار العمل إلى المجلد المستنسخ
 WORKDIR /root/The-LoliBot-MD
 
-COPY ./root/The-LoliBot-MD
+# نسخ الملفات إلى المجلد المحدد
+COPY . /root/The-LoliBot-MD
 
+# تثبيت الحزم باستخدام Yarn
 RUN yarn install
 
+# تشغيل التطبيق
 CMD ["node", "index.js"]
