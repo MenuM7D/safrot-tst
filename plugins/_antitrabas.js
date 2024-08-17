ed0f3bf39574fb9310dc63ed5a01da3233452cb0
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-08-17 19:25:19 +0300

    Update _antitrabas.js

--- plugins/_antitrabas.js
+++ plugins/_antitrabas.js
@@ -10,19 +10,19 @@ let bot = global.db.data.settings[this.user.jid] || {}
 let delet = m.key.participant
 let bang = m.key.id
 let name = await conn.getName(m.sender)
-let fakemek = {key: {participant: "0@s.whatsapp.net","remoteJid": "0@s.whatsapp.net"},"message": {"groupInviteMessage": {"groupJid": "51995386439-1616969743@g.us","inviteCode": "m","groupName": "P", "caption": 'ʟᴏʟɪʙᴏᴛ-ᴍᴅ', 'jpegThumbnail': null}}}
-if (chat.antiTraba && m.text.length > 5000) { //Cantidad máxima de caracteres aceptados en un mensaje//
-if (isAdmin) return conn.sendMessage(m.chat, { text: `⚠️El administrador @${m.sender.split("@")[0]} acaba de enviar un texto que contiene muchos caracteres -.-!`, mentions: [m.sender] }, { quoted: fakemek })
-conn.sendButton(m.chat, `*[ ! ] Se detecto un mensaje que contiene muchos caracteres [ ! ]*\n`, `${isBotAdmin ? '' : 'No soy administrador, no puedo hacer nada :/'}`, null, [['ᴅᴇsᴀᴄᴛɪᴠᴀ ᴀɴᴛɪᴛʀᴀʙᴀ', `/disable antitraba`]], null, null, m)
-//await conn.sendButton(m.chat, `*[ ! ] Se detecto un mensaje que contiene muchos caracteres [ ! ]*\n`, `${isBotAdmin ? '' : 'No soy administrador, no puedo hacer nada :/'}`, author, ['[  ]', usedPrefix+'apagar antitraba'], fakemek )
+let fakemek = {key: {participant: "0@s.whatsapp.net","remoteJid": "0@s.whatsapp.net"},"message": {"groupInviteMessage": {"groupJid": "51995386439-1616969743@g.us","inviteCode": "m","groupName": "P", "caption": '𝙎𝙖𝙛𝙧𝙤𝙩-𝘽𝙤𝙩', 'jpegThumbnail': null}}}
+if (chat.antiTraba && m.text.length > 5000) { //الحد الأقصى لعدد الحروف المقبولة في رسالة//
+if (isAdmin) return conn.sendMessage(m.chat, { text: `⚠️الأدمن @${m.sender.split("@")[0]} لسه باعت رسالة فيها حروف كتير -.-!`, mentions: [m.sender] }, { quoted: fakemek })
+conn.sendButton(m.chat, `*[ ! ] تم اكتشاف رسالة فيها حروف كتير [ ! ]*\n`, `${isBotAdmin ? '' : 'أنا مش أدمن، مش عارف أعمل حاجة :/'}`, null, [['إلغاء تفعيل الحماية من الرسائل الطويلة', `/disable antitraba`]], null, null, m)
+//await conn.sendButton(m.chat, `*[ ! ] تم اكتشاف رسالة فيها حروف كتير [ ! ]*\n`, `${isBotAdmin ? '' : 'أنا مش أدمن، مش عارف أعمل حاجة :/'}`, author, ['[  ]', usedPrefix+'apagar antitraba'], fakemek )
 if (isBotAdmin) {
 conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 setTimeout(() => { 
-conn.sendMessage(m.chat, { text: `Marcar el chat como leido ✓\n${"\n".repeat(400)}\n• El número : wa.me/${m.sender.split("@")[0]}\n• Alias : ${name}\n‼️Acaba de enviar un texto que contiene muchos caracteres que puede ocasionar fallos en los dispositivos`, mentions: [m.sender] }, { quoted: fakemek })
+conn.sendMessage(m.chat, { text: `تم تحديد الشات كمقروء ✓\n${"\n".repeat(400)}\n• الرقم : wa.me/${m.sender.split("@")[0]}\n• الاسم : ${name}\n‼️لسه باعت رسالة فيها حروف كتير اللي ممكن تسبب مشاكل في الأجهزة`, mentions: [m.sender] }, { quoted: fakemek })
 }, 0)
 setTimeout(() => { 
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-}, 1000)} else if (!bot.restrict) return m.reply(`Este comando esta desactivado por mi jefe`)
+}, 1000)} else if (!bot.restrict) return m.reply(`الأمر ده متعطل من رئيسي`)
 }
 return !0
-} 
+  }
