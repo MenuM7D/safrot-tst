ae667eccd44df68b89632a96e25c3152d6438923
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-08-17 19:37:32 +0300

    Update _simsimi.js

--- plugins/_simsimi.js
+++ plugins/_simsimi.js
@@ -6,14 +6,14 @@ const chat = global.db.data.chats[m.chat];
 if (chat.simi) {
 if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return;
 let textodem = m.text;
-if (m.text.includes('serbot') || m.text.includes('bots')|| m.text.includes('jadibot')|| m.text.includes('menu')|| m.text.includes('play')|| m.text.includes('play2') || m.text.includes('playdoc') || m.text.includes('tiktok') || m.text.includes('facebook') || m.text.includes('menu2') ||  m.text.includes('infobot') || m.text.includes('estado') ||  m.text.includes('ping') ||  m.text.includes('instalarbot') ||  m.text.includes('sc') ||  m.text.includes('sticker') ||  m.text.includes('s') || m.text.includes('wm') ||  m.text.includes('qc')) return
+if (m.text.includes('serbot') || m.text.includes('bots') || m.text.includes('jadibot') || m.text.includes('menu') || m.text.includes('play') || m.text.includes('play2') || m.text.includes('playdoc') || m.text.includes('tiktok') || m.text.includes('facebook') || m.text.includes('menu2') || m.text.includes('infobot') || m.text.includes('estado') || m.text.includes('ping') || m.text.includes('instalarbot') || m.text.includes('sc') || m.text.includes('sticker') || m.text.includes('s') || m.text.includes('wm') || m.text.includes('qc')) return
 try {
 await conn.sendPresenceUpdate('composing', m.chat)
 let gpt = await fetch(`https://deliriusapi-official.vercel.app/tools/simi?text=${encodeURIComponent(textodem)}`)
 let res = await gpt.json() 
 await m.reply(res.data.message) 
 } catch {
-/*SI DA ERROR USARA ESTA OTRA OPCION DE API DE IA QUE RECUERDA EL NOMBRE DE LA PERSONA*/
+/*لو حصل خطأ، هيستخدم API تاني يتذكر اسم الشخص*/
 if (textodem.includes('Hola')) textodem = textodem.replace('Hola', 'Hello');
 if (textodem.includes('hola')) textodem = textodem.replace('hola', 'hello');
 if (textodem.includes('HOLA')) textodem = textodem.replace('HOLA', 'HELLO');
