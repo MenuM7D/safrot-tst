920afb8fec9a6aa347bb6ccd27c14ca5f72ed60e
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-08-17 18:51:20 +0300

    Update تخفيض.js

--- plugins/تخفيض.js
+++ plugins/تخفيض.js
@@ -1,38 +1,46 @@
-var handler = async (m, { conn,usedPrefix, command, text }) => {
+var handler = async (m, { conn, usedPrefix, command, text }) => {
+    var number;
 
-if (isNaN(text) && !text.match(/@/g)){
-	
-} else if (isNaN(text)) {
-var number = text.split`@`[1]
-} else if (!isNaN(text)) {
-var number = text
+    if (isNaN(text) && !text.match(/@/g)){
+        return conn.reply(m.chat, `🧚🏼‍♂️ *\`『 الرقم غلط أو غير معرف 』\`*`, m);
+    } else if (isNaN(text)) {
+        number = text.split`@`[1];
+    } else if (!isNaN(text)) {
+        number = text;
+    }
+
+    if (!text && !m.quoted) 
+        return conn.reply(m.chat, `🧚🏼‍♂️ *\`『 امنشن للمشرف الي هتنزلو 』\` *`, m);
+
+    if (number.length > 13 || (number.length < 11 && number.length > 0)) 
+        return conn.reply(m.chat, `🧚🏼‍♂️ *\`『 الرقم غلط 』\`*`, m);
+
+    var user;
+    try {
+        if (text) {
+            user = number + '@s.whatsapp.net';
+        } else if (m.quoted && m.quoted.sender) {
+            user = m.quoted.sender;
+        } else if (m.mentionedJid) {
+            user = number + '@s.whatsapp.net';
+        } 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        if (user) {
+            await conn.groupParticipantsUpdate(m.chat, [user], 'demote');
+            conn.reply(m.chat, `🚯 *\`『 كنت ادمن فاشل 』\`*`, m);
+        }
+    }
 }
 
-if (!text && !m.quoted) return conn.reply(m.chat, `🧚🏼‍♂️ *\`『 امنشن للمشرف الي هتنزلو 』\` *`, m, fake, )
-if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `🧚🏼‍♂️ *\`『 الرقم غلط 』\`*`, m, fake, )
-  
-try {
-if (text) {
-var user = number + '@s.whatsapp.net'
-} else if (m.quoted.sender) {
-var user = m.quoted.sender
-} else if (m.mentionedJid) {
-var user = number + '@s.whatsapp.net'
-} 
-} catch (e) {
-} finally {
-conn.groupParticipantsUpdate(m.chat, [user], 'demote')
-conn.reply(m.chat, `🚯 *\`『 كنت ادمن فاشل 』\`*`, m, fake, )
-}
+handler.help = ['demote'];
+handler.tags = ['grupo'];
+handler.command = ['خفض', 'تخفيض'];
 
-}
-handler.help = ['demote']
-handler.tags = ['grupo']
-handler.command = ['خفض', 'تخفيض']
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+handler.fail = null;
 
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
-handler.fail = null
-
-export default handler
+export default handler;
