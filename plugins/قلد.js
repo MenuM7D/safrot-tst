4b2fcda8e7e9cf54ce595c0d8b37820c21a0d213
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-08-20 11:18:18 +0300

    Create قلد.js

---
+++ plugins/قلد.js
@@ -0,0 +1,17 @@
+let handler = async (m, { conn, text, participants, isAdmin, isOwner, usedPrefix, command }) => {
+  try {
+    // التأكد من وجود رسالة مقتبسة للرد عليها
+    if (!m.quoted) return conn.reply(m.chat, '*\`『 اعمل ريب علي الرساله الي هقلدها🧚🏻‍♂️ 』\`*', m)
+    
+    // إعادة إرسال الرسالة المقتبسة
+    await conn.sendMessage(m.chat, { forward: m.quoted.fakeObj }, { quoted: m })
+  } catch {
+    await m.react('✖️')
+  }
+}
+
+handler.help = ['قلد']
+handler.tags = ['tools']
+handler.command = ['قلد']
+
+export default handler
