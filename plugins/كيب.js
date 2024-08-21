c6af451acbd2ecc75cff63e71887db1afc439c72
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-08-21 18:20:14 +0300

    Update and rename قلد.js to كيب.js

--- plugins/كيب.js
+++ plugins/كيب.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, usedPrefix, command }) => {
   try {
     // التأكد من وجود رسالة مقتبسة للرد عليها
-    if (!m.quoted) return conn.reply(m.chat, '*\`『 اعمل ريب علي الرساله الي هقلدها🧚🏻‍♂️ 』\`*', m)
+    if (!m.quoted) return conn.reply(m.chat, '*\`『 اعمل ريب ع الرساله معا الامر🧚🏻‍♂️ 』\`*', m)
     
     // إعادة إرسال الرسالة المقتبسة
     await conn.sendMessage(m.chat, { forward: m.quoted.fakeObj }, { quoted: m })
@@ -12,6 +12,6 @@ let handler = async (m, { conn, text, participants, isAdmin, isOwner, usedPrefix
 
 handler.help = ['قلد']
 handler.tags = ['tools']
-handler.command = ['قلد|كيب']
+handler.command = ['كيب']
 
 export default handler
