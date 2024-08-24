a6aebe8c0e250adc2ebef4b59460ddb21d5f408f
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-08-24 16:14:28 +0300

    Update طرد.js

--- plugins/طرد.js
+++ plugins/طرد.js
@@ -1,20 +1,37 @@
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
-	
-let kickte = `*\`『 اعمل ريب او منشن للي هيطرد🧚🏼‍♂️ 』\`*`
 
-if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
-let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-let owr = m.chat.split`-`[0]
-await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`*\`『😹 تم  وديته الجحيم 』\`*`) 
+    let developers = ['201115618853@s.whatsapp.net'];   
 
+    let kickte = '*\`『 اعمل ريب او منشن للي عايز تطردو🧚🏻‍♂️ 』\`*';
+
+    
+    if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte) });
+
+    let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
+
+
+    if (developers.includes(user)) {
+        return m.reply('*\`『 احا عايز تطرد مطوري 🥺 』\`*');
+    }
+
+
+    await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
+
+   
+    m.reply('*\`『 تم الطرد وديتو الجحيم😹 』\`*');
+
+ 
+    let admins = participants.filter(participant => participant.admin).map(participant => participant.id);
+    let alertMessage = `*🧚🏻‍♂️┃ تم طرد ${user} من المجموعة*`;
+
+    conn.sendMessage(m.chat, { text: alertMessage, mentions: admins });
 }
 
-handler.help = ['طرد']
-handler.tags = ['group']
-handler.command = ['kick', 'طرد'] 
-handler.admin = true
-handler.group = true
-handler.botAdmin = true
+handler.help = ['kick @user'];
+handler.tags = ['group'];
+handler.command = ['kick', 'طرد'];
+handler.admin = true;
+handler.group = true;
+handler.botAdmin = true;
 
-export default handler
+export default handler;
