48b4c8b8ec7b0896cb73fac65de7825ca69feafe
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-08-26 10:24:09 +0300

    Update تخيل.js

--- plugins/تخيل.js
+++ plugins/تخيل.js
@@ -1,14 +1,31 @@
-import { googleImage } from '@bochilteam/scraper'
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `*\`『 اكتب الي متخيلو معا الامر 』\`*`
-if (m.text.includes('gore') || m.text.includes('cp')|| m.text.includes('porno')|| m.text.includes('Gore')|| m.text.includes('rule')|| m.text.includes('CP')|| m.text.includes('Rule34')) return m.reply('[❗خطاء❗] لا يمكنني إرسال هذا المحتوى ، المجموعة محظورة \n إذا كنت مشرفًا وتريد تنشيطها ، اخبر المطور')  
-const res = await googleImage(text)
-let image = await res.getRandom()
-let link = image
-conn.sendFile(m.chat, link, 'error.jpg', `🔎 *النتيجه ل:* ${text}\n🔗 *من* ${link}\n🌎 *محرك البحث:* جوجل`, m)}
-//let captionn = `🔎 *𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾 𝙳𝙴:* ${text}\n🔗 *𝙻𝙸𝙽𝙺* ${link}\n🌎 *𝙱𝚄𝚂𝙲𝙰𝙳𝙾𝚁:* Google`
-//conn.sendButton(m.chat, captionn, author, link, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `#imagen ${text}`]], m)}
-handler.help = ['gimage <query>', 'imagen <query>']
-handler.tags = ['internet', 'tools']
-handler.command = /^(gimage|image|تخيل|imagen)$/i
-export default handler
+import fetch from 'node-fetch';
+import translate from '@vitalets/google-translate-api';
+
+const handler = async (m, { command, usedPrefix, conn, args, text }) => {
+
+await conn.sendMessage(m.chat, { react: { text: '🔍', key: m.key } });
+
+let txt = text;
+let lang = 'en';
+if (!text && m.quoted && m.quoted.text) txt = m.quoted.text;
+
+try{
+
+const result = await translate(`${txt}`, {to: lang, autoCorrect: true});
+
+let img = `https://api-xovvip.vercel.app/text2img?text=${encodeURIComponent(result.text)}`;
+
+await conn.sendMessage(m.chat, { react: { text: '⏳', key: m.key } });
+
+await conn.sendMessage(m.chat, {image: {url: img}}, {quoted: m});
+ 
+
+ await conn.sendMessage(m.chat, { react: { text: '👌🏻', key: m.key } });
+ 
+ } catch {
+ await conn.sendMessage(m.chat, { react: { text: '❌', key: m.key } });
+ }
+ };
+ 
+ handler.command = /^(تخيل)$/i;
+export default handler;
