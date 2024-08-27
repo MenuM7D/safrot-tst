3698ba1da4de8ea95b68d290657814b20ee1fe8e
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-08-27 13:25:31 +0300

    Update طرد.js

--- plugins/طرد.js
+++ plugins/طرد.js
@@ -4,27 +4,21 @@ let handler = async (m, { conn, participants, usedPrefix, command }) => {
 
     let kickte = '*\`『 اعمل ريب او منشن للي عايز تطردو🧚🏻‍♂️ 』\`*';
 
-    
     if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte) });
 
     let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
-
 
     if (developers.includes(user)) {
         return m.reply('*\`『 احا عايز تطرد مطوري 🥺 』\`*');
     }
 
-
     await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
 
-   
     m.reply('*\`『 تم الطرد وديتو الجحيم😹 』\`*');
 
- 
     let admins = participants.filter(participant => participant.admin).map(participant => participant.id);
-    let alertMessage = `*🧚🏻‍♂️┃ تم طرد ${user} من المجموعة*`;
 
-    conn.sendMessage(m.chat, { text: alertMessage, mentions: admins });
+    conn.sendMessage(m.chat, { mentions: admins });
 }
 
 handler.help = ['kick @user'];
