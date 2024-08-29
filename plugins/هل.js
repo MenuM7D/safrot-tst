3792301cb585baa8c4889d0c264d7a5f983944b7
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-08-29 04:18:21 +0300

    Update هل.js

--- plugins/هل.js
+++ plugins/هل.js
@@ -1,22 +1,20 @@
- let handler = async (m, { text, conn, command, usedPrefix }) => {
+import util from 'util'
+import path from 'path'
+let user = a => '@' + a.split('@')[0]
+function handler(m, { groupMetadata, command, conn, text, usedPrefix}) {
+if (!text) throw `*أدخــل الـسـؤال !*`
+let ps = groupMetadata.participants.map(v => v.id)
+let a = ps.getRandom()
+let x = `${pickRandom(['احــتـمـال قـلـيـل' ,'نــعم بـالـتـأكـيد', 'لا أعـتـقـد', 'مــستـحـيــل'])}`
+let l = Math.floor(Math.random() * x.length);
+let top = `*هــل ${text}*
 
-const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
-
-if (!text) return conn.sendMessage(m.chat, {text: `*@${m.sender.split('@')[0]} أدخل السؤال أولا يا صديقي*
-*مثال: ${usedPrefix + command} البوت يحبني*
-`.trim(), mentions: [m.sender] }, { quoted: fkontak2 });
-
-let cap = `
-*╭──────≼𝙎𝙖𝙛𝙧𝙤𝙩-𝘽𝙤𝙩≽──────╮*
-*┆ السائل: @${m.sender.split('@')[0]}*
-*┆ السؤال: ${command} ${text}*
-*┆ الاجابة: ${['نعم','ممكن','في الاغلب نعم','في الاغلب لا','لا','مستحيل'].getRandom()}*
-*╰─────𝙎𝙖𝙛𝙧𝙤𝙩-𝘽𝙤𝙩≽──────╯*
-`.trim();
-
-await conn.sendMessage(m.chat, {text: cap, mentions: [m.sender] }, { quoted: fkontak2 });
-};
-handler.help = ['pregunta <texto>?']
-handler.tags = ['kerang']
-handler.command = /^هل$/i
+*الــأجــابـه :* ${x}`.trim()
+conn.sendFile (m.reply (top, null, { mentions: [a]}))}
+handler.help = handler.command = ['هل']
+handler.tags = ['fun']
+handler.group = true
+handler.limit = 0
 export default handler
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]}
