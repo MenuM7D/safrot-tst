6b0e43072b20d383f86744bef67c9a029acba58a
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-08-31 13:57:44 +0300

    Update ملصق.js

--- plugins/ملصق.js
+++ plugins/ملصق.js
@@ -1,24 +1,36 @@
 import uploadImage from '../lib/uploadImage.js'
 import { sticker } from '../lib/sticker.js'
+
 let handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!text) return m.reply('*\`『 يرجى إضافة النص! 』\`*')
+    
     let [فوق, تحت] = text.split`|`
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
+    
     if (!mime) return m.reply('*\`『 اعمل ريب ع الصوره الي عايز تخليها ملصق🧚🏻‍♂️ 』\`*')
+    
     try {
-    if (!/image\/(jpe?g|png)/.test(mime)) return await m.react('✖️')
-    await m.react('🕓')
-    let img = await q.download()
-    let url = await uploadImage(img)
-    let meme = `https://api.memegen.link/images/custom/${encodeURIComponent(فوق ? فوق : '')}/${encodeURIComponent(تحت ? تحت : '')}.png?background=${url}`
-    let stiker = await sticker(false, meme, global.packname, global.author)
-    if (stiker) await conn.sendFile(m.chat, stiker, '', global.author, m, '', { asSticker: 1 })
-    await m.react('✅')
-} catch {
-await m.react('✖️')
-}}
+        if (!/image\/(jpe?g|png)/.test(mime)) return await m.react('✖️')
+        
+        await m.react('🕓')
+        
+        let img = await q.download()
+        let url = await uploadImage(img)
+        let meme = `https://api.memegen.link/images/custom/${encodeURIComponent(فوق ? فوق : '')}/${encodeURIComponent(تحت ? تحت : '')}.png?background=${url}`
+        let stiker = await sticker(false, meme, global.packname, global.author)
+        
+        if (stiker) await conn.sendFile(m.chat, stiker, '', global.author, m, '', { asSticker: 1 })
+        
+        await m.react('✅')
+    } catch (error) {
+        await m.react('✖️')
+        console.error(error)
+    }
+}
+
 handler.help = ['صورة_مضحكة *<نص>*']
 handler.tags = ['ستيكر']
 handler.command = /^(ملصق|لملصق)$/i
- 
+
 export default handler
