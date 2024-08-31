7b4b5f5649f4da41e4b2e4875d1f4a765573aa2d
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-08-31 19:02:41 +0300

    Update موت.js

--- plugins/موت.js
+++ plugins/موت.js
@@ -1,16 +1,16 @@
 let handler = async (m, { conn }) => {
     // الحصول على JID الشخص المستهدف
-    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    let who = m.quoted ? m.quoted.sender : (m.mentionedJid && m.mentionedJid[0]) ? m.mentionedJid[0] : (m.fromMe ? conn.user.jid : m.sender)
 
     // الحصول على اسم الشخص المستهدف
-    let name = conn.getName(who)
+    let name = await conn.getName(who) // استخدم await هنا للحصول على الاسم
 
     // الحصول على صورة الملف الشخصي أو استخدام الصورة الافتراضية
     let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './mob.jpg')
 
     // إرسال الصورة مع الرسالة
     conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/overlay/wasted', {
-      avatar: pp, 
+        avatar: pp,
     }), 'waste.png', `*⎔❯╼╾╾╃❖⟪ 🍥 ⟫❖╃╼╾╾❮⎔*
 *⌝🪦┇لقد مات┇🪦⌞*
 *⎔❯╼╾╾╃❖⟪ 🍥 ⟫❖╃╼╾╾❮⎔*
