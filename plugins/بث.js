a806240010d03ce0c3ad00281cd132a8e7c6c7b2
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-09-03 20:22:39 +0300

    Update بث.js

--- plugins/بث.js
+++ plugins/بث.js
@@ -1,1 +1,61 @@
-يلا يا حرامي الملف ده فاضي عمك سفروت يكسمك😂
+/**
+من فضلكم، ياريت تحافظوا على حقوق ملكية Safrot-MD،
+لو عايزين تضيفوا بيانات أو شكر خاص بيكم، مافيش مشكلة. لكن لو سمحتوا، متشيلوش الفقرات اللي موجودة من Safrot-MD. شكرًا
+**/
+let handler = async (m, { conn, command, usedPrefix }) => {
+    let name = await conn.getName(m.sender)
+    let usuario = `${m.sender.split("@")[0]}`
+    let aa = `${usuario}@s.whatsapp.net`
+    let _uptime = process.uptime() * 1000
+    let _muptime = 0
+    if (process.send) { 
+        process.send('uptime')
+        _muptime = await new Promise(resolve => { 
+            process.once('message', resolve) 
+            setTimeout(resolve, 1000) 
+        }) * 1000
+    }
+
+    let fkontak = { 
+        "key": { 
+            "participants": "0@s.whatsapp.net", 
+            "remoteJid": "status@broadcast", 
+            "fromMe": false, 
+            "id": "Halo" 
+        }, 
+        "message": { 
+            "contactMessage": { 
+                "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` 
+            }
+        }, 
+        "participant": "0@s.whatsapp.net" 
+    }
+
+    let uptime = clockString(_uptime)
+    let estado = `${pickRandom([
+        '*\`『 ااستر يار🧚🏻‍♂️ 』\`*',
+        '*\`『 اسمي سفروت🧚🏻‍♂️ 』\`*',
+        '*\`『 مش عارف تقول سفروت🧚🏻‍♂️ 』\`*',
+        '*\`『 انت الي بوت🧚🏻‍♂️ 』\`*',
+        '*\`『 انت الي بوت اسمي سفروت🧚🏻‍♂️ 』\`*'
+    ])}`
+    
+    await conn.sendMessage(m.chat, {text: estado, mentions: [m.sender]}, {quoted: fkontak})
+}
+
+handler.help = ['estado']
+handler.tags = ['main']
+handler.command = /^(بوت(us)?)$/i
+export default handler
+
+function pickRandom(list) {
+    if (list.length === 0) return 'لا يوجد حالات متاحة'
+    return list[Math.floor(Math.random() * list.length)]
+}
+
+function clockString(ms) {
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
