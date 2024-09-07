e5c4dc96a427e61e70e7d6159a018aea6a599507
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-09-07 04:49:58 +0300

    Update بحقوك.js

--- plugins/بحقوك.js
+++ plugins/بحقوك.js
@@ -1,29 +1,23 @@
-import {addExif} from '../lib/sticker.js';
-
-const handler = async (m, {conn, text}) => {
-    if (!m.quoted) return conn.reply(m.chat, '*\`『 اعمل ريب ع الملصق الي هتخلي باسمك 』\`*🧚🏼‍♂️', m, {contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null, title: wm, body: '', previewType: 0, thumbnail: img.getRandom(), sourceUrl: redes.getRandom()}}});
-
-    let stiker = false;
-    try {
-        let [packname, ...author] = text.split('|');
-        author = (author || []).join('|');
-        const mime = m.quoted.mimetype || '';
-        if (!/webp/.test(mime)) return conn.reply(m.chat, '*\`『 اعمل ريب ع الملصق الي هتخلي باسمك』\`*🧚🏼‍♂️', m, {contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null, title: wm, body: '', previewType: 0, thumbnail: img.getRandom(), sourceUrl: redes.getRandom()}}});
-        const img = await m.quoted.download();
-        if (!img) return conn.reply(m.chat, '*في حاجه غلط غير الملصق او عيد المحوله*❗', m, {contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null, title: wm, body: '', previewType: 0, thumbnail: img.getRandom(), sourceUrl: redes.getRandom()}}});
-        stiker = await addExif(img, packname || global.packname, author || global.author);
-    } catch (e) {
-        console.error(e);
-        if (Buffer.isBuffer(e)) stiker = e;
-    } finally {
-        if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, true, {contextInfo: {'forwardingScore': 200, 'isForwarded': false, externalAdReply: {showAdAttribution: false, title: wm, body: '', mediaType: 2, sourceUrl: redes.getRandom(), thumbnail: img.getRandom()}}}, {quoted: m});
-        else return conn.reply(m.chat, '*في حاجه غلط غير الملصق او عيد المحوله*❗', m, {contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null, title: wm, body: '', previewType: 0, thumbnail: img.getRandom(), sourceUrl: redes.getRandom()}}});
-    }
-}
-
-handler.help = ['سرقه'];
-handler.tags = ['sticker'];
-handler.command = /^take|بحقوق|سرقه|wm$/i;
-//handler.register = true;
-
-export default handler;
+import { addExif } from '../lib/sticker.js'
+let handler = async (m, { conn, text }) => {
+if (!m.quoted) throw '*قم بالرد على الملصق الذي تريد إضافة حزمة إليه*'
+let stiker = false
+try {
+let [packname, ...author] = text.split('|')
+author = (author || []).join('|')
+let mime = m.quoted.mimetype || ''
+if (!/webp/.test(mime)) throw '*قم بالرد على الملصق الذي تريد إضافة حزمة إليه*'
+let img = await m.quoted.download()
+if (!img) throw '*امنح الملصق الذي تريده لإضافة حزمة واسم*'
+stiker = await addExif(img, packname || '', author || '')
+} catch (e) {
+console.error(e)
+if (Buffer.isBuffer(e)) stiker = e
+} finally {
+if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, false, { asSticker: true })
+else throw '*آسف ، شيء ما خاطئ ..تحقق من أنك استجابت لملصق وأضفت اسم حزمة واسم حزمة*'
+}}
+handler.help = ['wm <packname>|<author>']
+handler.tags = ['sticker']
+handler.command = /^سرقه|سرقة$/i
+export default handler
