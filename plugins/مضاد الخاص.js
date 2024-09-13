6143e90a0ce4903d03952af3e1562de54d18ce25
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-09-13 11:22:25 +0300

    Update مضاد الخاص.js

--- plugins/مضاد الخاص.js
+++ plugins/مضاد الخاص.js
@@ -1,4 +1,4 @@
-export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
+    export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
     if (m.isBaileys && m.fromMe) return !0;
     if (m.isGroup) return !1;
     if (!m.message) return !0;
@@ -7,12 +7,23 @@ export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }
     const chat = global.db.data.chats[m.chat];
     const bot = global.db.data.settings[this.user.jid] || {};
 
+    // استخدام متغير لتتبع عدد التحذيرات
+    if (!chat.warnCount) chat.warnCount = 0;
+
     if (bot.antiPrivate && !isOwner && !isROwner) {
-        await m.reply(`*[⚠️ تحذير] @${m.sender.split`@`[0]}، يمنع استخدام الخاص مع البوت. إذا استمريت، سيتم حظرك!*`, false, { mentions: [m.sender] });
-        await m.reply(`*[⚠️ تحذير أخير] @${m.sender.split`@`[0]}، دي آخر فرصة. التحدث في الخاص يؤدي للحظر.*\n\n*ادخل جروب الدعم لو محتاج أي مساعدة: 『 https://chat.whatsapp.com/ClNQxTnKyFx6eZnJcvqsPY 』*`, false, { mentions: [m.sender] });
-        await m.reply(`*[❗] مرحبًا @${m.sender.split`@`[0]}، تم حظرك لأنك خالفت التعليمات وتحدثت في الخاص.*\n\n*اذا كنت تريد تجربة البوت ادخل جروب الدعم 『 https://chat.whatsapp.com/ClNQxTnKyFx6eZnJcvqsPY 』*\n\n*لو حابب تتواصل مع مطور البوت ده رقمه 『‏‪『https://wa.me/201115618853*`, false, { mentions: [m.sender] });
-        await this.updateBlockStatus(m.chat, 'block');
+        chat.warnCount += 1;
+
+        if (chat.warnCount === 1) {
+            await m.reply(`*[⚠️ تحذير] @${m.sender.split`@`[0]}، يمنع استخدام الخاص مع البوت. إذا استمريت، سيتم حظرك!*`, false, { mentions: [m.sender] });
+        } else if (chat.warnCount === 2) {
+            await m.reply(`*[⚠️ تحذير ثاني] @${m.sender.split`@`[0]}، التحدث في الخاص مخالف. إذا استمريت سيتم حظرك!*`, false, { mentions: [m.sender] });
+        } else if (chat.warnCount === 3) {
+            await m.reply(`*[⚠️ تحذير أخير] @${m.sender.split`@`[0]}، دي آخر فرصة. التحدث في الخاص يؤدي للحظر.*\n\n*ادخل جروب الدعم لو محتاج أي مساعدة: 『 https://chat.whatsapp.com/ClNQxTnKyFx6eZnJcvqsPY 』*`, false, { mentions: [m.sender] });
+        } else if (chat.warnCount >= 4) {
+            await m.reply(`*[❗] مرحبًا @${m.sender.split`@`[0]}، تم حظرك لأنك خالفت التعليمات وتحدثت في الخاص.*\n\n*اذا كنت تريد تجربة البوت ادخل جروب الدعم 『 https://chat.whatsapp.com/ClNQxTnKyFx6eZnJcvqsPY 』*\n\n*لو حابب تتواصل مع مطور البوت ده رقمه 『‏‪『https://wa.me/201115618853*`, false, { mentions: [m.sender] });
+            await this.updateBlockStatus(m.chat, 'block');
+        }
     }
 
     return !1;
-}
+                          }
