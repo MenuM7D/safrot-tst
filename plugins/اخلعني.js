3c7aac6d1a068dec769500f117c0c081149e2783
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-09-14 15:37:57 +0300

    Create اخلعني.js

---
+++ plugins/اخلعني.js
@@ -0,0 +1,28 @@
+let toM = a => '@' + a.split('@')[0]
+
+Array.prototype.getRandom = function() {
+  return this[Math.floor(Math.random() * this.length)];
+}
+
+function handler(m, { groupMetadata }) {
+  let ps = groupMetadata.participants.map(v => v.id);
+  let a = ps.getRandom();
+  let b;
+  
+  do {
+    b = ps.getRandom();
+  } while (b === a);
+  
+  let user = m.sender;
+  
+  m.reply(`@${user.split('@')[0]}  *\`『 يلا ي حربوقه انتي متنفعيش تبقي مراتي 😹🐤 』\`*  ${toM(b)}, 😹`, null, {
+    mentions: [user, b]
+  });
+}
+
+handler.help = ['formarpareja'];
+handler.tags = ['main', 'fun'];
+handler.command = ['اخلعني'];
+handler.group = true;
+
+export default handler;
