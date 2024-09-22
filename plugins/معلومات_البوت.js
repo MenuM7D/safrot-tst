6dcdcf891637940097cd2835f56d03d05f33a7c7
safrot11 <safrot272@gmail.com>
2024-09-22 07:28:22 +0300

    Update معلومات_البوت.js

--- plugins/معلومات_البوت.js
+++ plugins/معلومات_البوت.js
@@ -1,8 +1,6 @@
 import db from '../lib/database.js'
-//import si from "systeminformation";
-import { cpus as _cpus, totalmem, freemem, platform, hostname, version, release, arch } from 'os'
+import { cpus as _cpus, totalmem, freemem, platform, hostname } from 'os'
 import speed from 'performance-now'
-import { performance } from 'perf_hooks'
 import { sizeFormatter } from 'human-readable'
 
 let format = sizeFormatter({
@@ -13,65 +11,32 @@ let format = sizeFormatter({
 })
 
 let handler = async (m, { conn, usedPrefix }) => {
-   let bot = global.db.data.settings[conn.user.jid]
-   let _uptime = process.uptime() * 1000
-   let uptime = (_uptime).toTimeString()
-   let totalreg = Object.keys(global.db.data.users).length
-   let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
-   let totalbots = Object.keys(global.db.data.settings).length
-   let totalStats = Object.values(global.db.data.stats).reduce((total, stat) => total + stat.total, 0)
-   const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
-   let totalchats = Object.keys(global.db.data.chats).length
-   let totalf = Object.values(global.plugins).filter(
-    (v) => v.help && v.tags
-  ).length
-   const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) //groups.filter(v => !v.read_only
-   let totaljadibot = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
-   const used = process.memoryUsage()
-   const cpus = _cpus().map(cpu => {
-      cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
-      return cpu
-   })
-   const cpu = cpus.reduce((last, cpu, _, { length }) => {
-      last.total += cpu.total
-      last.speed += cpu.speed / length
-      last.times.user += cpu.times.user
-      last.times.nice += cpu.times.nice
-      last.times.sys += cpu.times.sys
-      last.times.idle += cpu.times.idle
-      last.times.irq += cpu.times.irq
-      return last
-   }, {
-      speed: 0,
-      total: 0,
-      times: {
-         user: 0,
-         nice: 0,
-         sys: 0,
-         idle: 0,
-         irq: 0
-      }
-   })
-   let _muptime
-   if (process.send) {
-      process.send('uptime')
-      _muptime = await new Promise(resolve => {
-         process.once('message', resolve)
-         setTimeout(resolve, 1000)
-      }) * 1000
-   }
-//let ram = await si.mem(); 
-// let cpu = await si.cpuCurrentSpeed(); 
-// let disk = await si.fsSize(); 
- /*let json = { 
-   memory: formatSize(ram.free) + " de " + formatSize(ram.total), 
-   memory_used: formatSize(ram.used), 
-   cpu: cpu.avg + " Ghz", 
-   disk: formatSize(disk[0].available), 
- }; */  
-   let timestamp = speed()
-   let latensi = speed() - timestamp
-   let teks = `*≡ INFOBOT*
+    let bot = global.db.data.settings[conn.user.jid]
+    let _uptime = process.uptime()
+    let hours = Math.floor(_uptime / 3600);
+    let minutes = Math.floor((_uptime % 3600) / 60);
+    let seconds = Math.floor(_uptime % 60);
+    let uptime = `${hours}h ${minutes}m ${seconds}s`;
+
+    let totalreg = Object.keys(global.db.data.users).length
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered === true).length
+    let totalStats = Object.values(global.db.data.stats).reduce((total, stat) => total + stat.total, 0)
+    const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+    let totalchats = Object.keys(global.db.data.chats).length
+    let totalf = Object.values(global.plugins).filter(v => v.help && v.tags).length
+    const groupsIn = chats.filter(([id]) => id.endsWith('@g.us'))
+    let totaljadibot = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
+    const used = process.memoryUsage()
+    const cpus = _cpus().map(cpu => {
+        cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
+        return cpu
+    })
+    
+    const usedMemory = totalmem() - freemem();
+    
+    let timestamp = speed()
+    let latensi = speed() - timestamp
+    let teks = `*≡ 𝑺𝐴𝐹𝑅O𝑇-𝐵O𝑇*
 
 *معلومات*
 *▣ عدد الجروبات الكلي:* ${groupsIn.length}
@@ -83,25 +48,19 @@ let handler = async (m, { conn, usedPrefix }) => {
 *▣ إجمالي الاضافات:* ${totalf}
 *▣ السرعة:* ${latensi.toFixed(4)} مللي ثانية
 *▣ وقت النشاط:* ${uptime}
- 
+
 *▣ الأوامر المنفذة:* ${toNum(totalStats)}/${totalStats}
 *▣ الجروبات المسجلة:* ${toNum(totalchats)}/${totalchats}
 *▣ المستخدمين المسجلين:*  ${rtotalreg} من ${totalreg} مستخدم
 
 *≡ السيرفر*
 ▣ *اسم السيرفر:* ${hostname()}
-▣ *الرام المستخدمة:* ${format(totalmem() - freemem())} / ${format(totalmem())}
+▣ *الرام المستخدمة:* ${format(usedMemory)} / ${format(totalmem())}
 ▣ *المنصة:* ${platform()}`
-await conn.sendMessage(m.chat, { image: { url: "https://i.ibb.co/b54Ym1M/file.jpg", }, caption: teks,
-contextInfo: {
-externalAdReply: {
-title: `𝑺𝐴𝐹𝑅O𝑇-𝐵O𝑇`,
-sourceUrl: nna, 
-mediaType: 1,
-showAdAttribution: true,
-thumbnailUrl: img1,
-}}}, { quoted: m })
+
+    await conn.sendMessage(m.chat, { image: { url: "https://i.ibb.co/b54Ym1M/file.jpg" }, caption: teks }, { quoted: m })
 }
+
 handler.help = ['infobot']
 handler.tags = ['main']
 handler.command = /^(معلومات_البوت|informacionbot|infololi)$/i;
