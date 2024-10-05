98de5a6838c0fbf7323210fc5f23cf7574406a27
safrot11 <safrot272@gmail.com>
2024-10-06 02:13:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,70 +1,74 @@
-console.log('Iniciando 🚀🚀🚀') 
 import { join, dirname } from 'path'
-import { createRequire } from 'module'
+import { createRequire } from 'module';
 import { fileURLToPath } from 'url'
 import { setupMaster, fork } from 'cluster'
 import { watchFile, unwatchFile } from 'fs'
-import cfonts from 'cfonts'
+import cfonts from 'cfonts';
 import { createInterface } from 'readline'
 import yargs from 'yargs'
+import express from 'express'
+import path from 'path'
+
+// https://stackoverflow.com/a/50052194
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const require = createRequire(__dirname)
-const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
- 
-say('LoliBot-MD', {
-font: 'chrome',
-align: 'center',
-gradient: ['red', 'magenta']})
-say(`by: elrebelde21`, {
-font: 'console',
-align: 'center',
-gradient: ['red', 'magenta']});
 
-let isRunning = false
-/**
-* Start a js file
-* @param {String} file `path/to/file`
-*/
-function start(file) {
-if (isRunning) return
-isRunning = true
-const args = [join(__dirname, file), ...process.argv.slice(2)]
+say('SAFROT\nMD', {
+    font: 'slick',
+    align: 'center',
+    gradient: ['blue', 'gray']
+})
 
-setupMaster({
-exec: args[0],
-args: args.slice(1)})
-const p = fork()
-p.on('message', (data) => {
-//console.log('╭--------- - - - ✓\n┆ ✅ TIEMPO DE ACTIVIDAD ACTUALIZADA\n╰-------------------- - - -', data)
-switch (data) {
-case 'reset':
-p.process.kill()
-isRunning = false
-start.apply(this, arguments)
-break
-case 'uptime':
-p.send(process.uptime())
-break
+say(`Made By Safrot`, {
+    font: 'console',
+    align: 'center',
+    gradient: ['blue', 'gray']
+})
+
+var isRunning = false
+
+async function start(file) {
+    if (isRunning) return
+    isRunning = true
+    let args = [join(__dirname, file), ...process.argv.slice(2)]
+    say([process.argv[0], ...args].join(' '), {
+        font: 'console',
+        align: 'center',
+        gradient: ['blue', 'gray']
+    })
+    setupMaster({
+        exec: args[0],
+        args: args.slice(1),
+    })
+    let p = fork()
+    p.on('message', data => {
+        switch (data) {
+            case 'reset':
+                p.process.kill()
+                isRunning = false
+                start.apply(this, arguments)
+                break
+            case 'uptime':
+                p.send(process.uptime())
+                break
+        }
+    })
+
+    p.on('exit', (_, code) => {
+        isRunning = false
+        console.error('⚠️ ERROR ⚠️ >> ', code)
+        start('main.js'); //
+
+        if (code === 0) return
+        watchFile(args[0], () => {
+            unwatchFile(args[0])
+            start(file)
+        })
+    })
+
+    setInterval(() => {}, 1000)
 }
-})
-p.on('exit', (_, code) => {
-isRunning = false;
-console.error('⚠️ Error Inesperado ⚠️', code)
-p.process.kill()
-isRunning = false
-start.apply(this, arguments)
-if (process.env.pm_id) {
-process.exit(1)
-} else {
-process.exit()
-}})
-const opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
-if (!opts['test']) {
-if (!rl.listenerCount()) {
-rl.on('line', (line) => {
-p.emit('message', line.trim())
-})
-}}}
-start('main.js')
+
+start('remcham.js')
