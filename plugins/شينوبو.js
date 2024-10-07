bed62c70a270991ac564edbc88012ea488f8682a
safrot11 <safrot272@gmail.com>
2024-10-08 01:45:43 +0300

    Update شينوبو.js

--- plugins/شينوبو.js
+++ plugins/شينوبو.js
@@ -1,21 +1,20 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-    await m.react('🕓')
+    await m.react('🕓') // إرسال رد فعل الساعة
     try {
-        let res = await fetch('https://api.waifu.pics/sfw/shinobu')
+        let res = await fetch('https://api.waifu.pics/sfw/shinobu') // جلب الصورة من API
         if (!res.ok) return
         let json = await res.json()
         if (!json.url) return
-        await conn.sendFile(m.chat, json.url, 'thumbnail.jpg', 'صورة شينوبو', m)
-        await m.react('✅')
+        await conn.sendButton(m.chat, '🐦شينوبو', '𝐒𝐀𝐅𝐑𝐎𝐓-𝐁𝐎𝐓', json.url, [['🧚🏻‍♂️ صوره تاني', `${usedPrefix + command}`]], m)
+        await m.react('✅') // إرسال رد فعل نجاح
     } catch {
-        await m.react('✖️')
+        await m.react('✖️') // إرسال رد فعل فشل في حالة وجود خطأ
     }
 }
 handler.help = ['شينوبو']
 handler.tags = ['صورة']
 handler.command = ['شينوبو']
-
 
 export default handler
