8e18e4419b96b06aa20e86cc255b24ba16483826
safrot11 <safrot272@gmail.com>
2024-10-08 13:31:15 +0300

    Update مراتي.js

--- plugins/مراتي.js
+++ plugins/مراتي.js
@@ -1,21 +1,24 @@
 import fetch from 'node-fetch'
-let handler = async (m, {
-    conn,
-    usedPrefix
-}) => {
+
+let handler = async (m, { conn, usedPrefix }) => {
     await conn.sendMessage(m.chat, {
         react: {
             text: '🧚🏼‍♂️',
             key: m.key,
         }
     })
+
     let res = await fetch('https://api.waifu.pics/sfw/waifu')
     if (!res.ok) throw await res.text()
     let json = await res.json()
     if (!json.url) throw 'Error!'
-    await conn.sendFile(m.chat, json.url, null, "*\`『 𝙎𝙖𝙛𝙧𝙤𝙩-𝘽𝙤𝙩 』\`*", m)
+
+    // إرسال الزر مع الصورة
+    await conn.sendButton(m.chat, "*وه 🐤*", author, json.url, [['صوره تاني 🧚🏻‍♂️', `${usedPrefix}مراتي`]], m)
 }
+
 handler.help = ['waifu']
 handler.tags = ['internet']
 handler.command = /^(مراتي)$/i
+
 export default handler
