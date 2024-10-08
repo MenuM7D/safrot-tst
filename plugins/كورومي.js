b4709ff9400dc3212d1ccabf6703ff0c3da1ff80
safrot11 <safrot272@gmail.com>
2024-10-08 15:49:52 +0300

    Create كورومي.js

---
+++ plugins/كورومي.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+let handler = async(m, { conn, usedPrefix, command }) => {
+    let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/anime-kurumi.json`)).data;
+    let url = await res[Math.floor(res.length * Math.random())];
+    await conn.sendButton(m.chat, "كـورومـي 😵", "𝐒𝐀𝐅𝐑𝐎𝐓-𝐁𝐎𝐓", url, [['🧚🏻‍♂️ صوره تاني', `${usedPrefix + command}`]], m);
+}
+
+handler.help = ['anna'];
+handler.tags = ['internet'];
+handler.command = /^(kurumi|كورومي)$/i;
+
+export default handler;
