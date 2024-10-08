4b7e67321b6516dedb8aeaae4b727da70d06792f
safrot11 <safrot272@gmail.com>
2024-10-08 10:56:23 +0300

    Create اينوري.js

---
+++ plugins/اينوري.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+let handler = async(m, { conn, usedPrefix, command }) => {
+    let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/anime-inori.json`)).data;
+    let url = await res[Math.floor(res.length * Math.random())];
+    await conn.sendButton(m.chat, "ايـنـوري 🥱", "𝐒𝐀𝐅𝐑𝐎𝐓-𝐁𝐎𝐓", url, [['🧚🏻‍♂️ صوره تاني', `${usedPrefix + command}`]], m);
+}
+
+handler.help = ['anna'];
+handler.tags = ['internet'];
+handler.command = /^(inori|اينوري)$/i;
+
+export default handler;
