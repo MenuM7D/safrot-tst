1540240d9e1d2c82fdc4ed30e182d86423ccac31
safrot11 <safrot272@gmail.com>
2024-10-09 22:03:51 +0300

    Update رد4.js

--- plugins/رد4.js
+++ plugins/رد4.js
@@ -20,72 +20,59 @@ handler.all = async function(m) {
   const randomReply = (replies) => replies[Math.floor(Math.random() * replies.length)];
 
   if (/^سفرووت|سفروت|سفروتي|يسفروت|سفريتو$/i.test(m.text) && !chat.isBanned) {
-    const replies = ['*قلبي🧚🏻‍♂️*', '*اي يحب🧚🏻‍♂️*', '*ارغي🐤*', '*معاك🧚🏻‍♂️*', '*قلب 🧚🏻‍♂️*', '*اي يسطا🧚🏻‍♂️*', '*يساتر🧚🏻‍♂️*', '*عيون سفروت💗*', '*معاك يزميلي😹💗*'];
-    conn.sendPresenceUpdate('recording', m.chat);
-    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: fk});
-    
+    const replies = ['*قلبي🧚🏻‍♂️*', '*اي يحب🧚🏻‍♂️*', '*ارغي🐤*', '*معاك🧚🏻‍♂️*', '*قلب سفروت 🧚🏻‍♂️*', '*اي يسطا🧚🏻‍♂️*', '*يساتر🧚🏻‍♂️*', '*عيون سفروت💗*', '*معاك يزميلي😹💗*'];
+    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: m});
+
   } else if (/^بوت|يبوت|يبوتي|بووت|بوووت$/i.test(m.text) && !chat.isBanned) {
     const replies = ['*اسمي سفروت🧚🏻‍♂️*', '*انت الي بوت🧚🏻‍♂️*', '*قول سفروت🧚🏻‍♂️*'];
-    conn.sendPresenceUpdate('recording', m.chat);
-    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: fk});
-    
+    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: m});
+
   } else if (/^لا انت بوت|انت بوت$/i.test(m.text) && !chat.isBanned) {
     const replies = ['*قولت سامي سفروت🐤*'];
-    conn.sendPresenceUpdate('recording', m.chat);
-    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: fk});
-    
+    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: m});
+
   } else if (/^بوت عبيط$/i.test(m.text) && !chat.isBanned) {
     const replies = ['*كسمك🙂*', '*انت الي عبيط🐤*', '*بس يهطل😹*'];
-    conn.sendPresenceUpdate('recording', m.chat);
-    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: fk});
+    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: m});
 
   } else if (/^كسمك بوت|ينعل كسم البوت$/i.test(m.text) && !chat.isBanned) {
     const replies = ['*علي كسم الي جبتك🫵🏻*'];
-    conn.sendPresenceUpdate('recording', m.chat);
-    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: fk});
+    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: m});
 
   } else if (/^سفروت بتبحبي|بوت بتحبني$/i.test(m.text) && !chat.isBanned) {
     const replies = ['*اها بحبني😹*', '*بكرهك😹💗*', '*بحب نفسي اكتر😹💗*', '*مش عارف😹🙂*'];
-    conn.sendPresenceUpdate('recording', m.chat);
-    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: fk});
+    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: m});
 
   } else if (/^خت خزوق|خزوقتني|ادتني خزوق|لسه متخزوق|لسه مخزوقاني$/i.test(m.text) && !chat.isBanned) {
     const replies = ['*يمتخزوق😹🫵🏻*', '*المتخزوق اهو🫵🏻😹*'];
-    conn.sendPresenceUpdate('recording', m.chat);
-    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: fk});
+    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: m});
 
   } else if (/^🙂|🙂🙂|.🙂$/i.test(m.text) && !chat.isBanned) {
     const replies = ['*اتصدم🙂*', '*معلش الصدمه وحشه🙂*', '*حقك عليا🙂*'];
-    conn.sendPresenceUpdate('recording', m.chat);
-    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: fk});
+    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: m});
 
   } else if (/^نو ريب$/i.test(m.text) && !chat.isBanned) {
     const replies = ['*يمحني🐤*'];
-    conn.sendPresenceUpdate('recording', m.chat);
-    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: fk});
+    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: m});
 
   } else if (/^بس|سكتي البتاع ده|سكت البتات ده|سكتو$/i.test(m.text) && !chat.isBanned) {
     const replies = ['*لا مش هسكت😹🙂*'];
-    conn.sendPresenceUpdate('recording', m.chat);
-    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: fk});
+    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: m});
 
   } else if (/^هاي$/i.test(m.text) && !chat.isBanned) {
     const replies = ['*باي🐤*'];
-    conn.sendPresenceUpdate('recording', m.chat);
-    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: fk});
+    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: m});
 
   } else if (/^بتحبني$/i.test(m.text) && !chat.isBanned) {
     const replies = ['*بحب ابراهيم اكتر💗🐤*'];
-    conn.sendPresenceUpdate('recording', m.chat);
-    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: fk});
+    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: m});
 
   } else if (/^حد صاحي$/i.test(m.text) && !chat.isBanned) {
     const replies = ['*مش انا😹💗*'];
-    conn.sendPresenceUpdate('recording', m.chat);
-    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: fk});
+    m.conn.sendMessage(m.chat, {text: randomReply(replies)}, {quoted: m});
 
   }
-  
+
   return !0;
 };
 export default handler;
