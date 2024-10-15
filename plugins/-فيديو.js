21ad7562cf54f9d7f38bd5a6f7032660cdd310e9
safrot11 <safrot272@gmail.com>
2024-10-15 19:56:05 +0300

    Update -فيديو.js

--- plugins/-فيديو.js
+++ plugins/-فيديو.js
@@ -1,24 +1,28 @@
-.ee k import axios from 'axios'
-const {proto, generateWAMessageFromContent, prepareWAMessageMedia, generateWAMessageContent, getDevice} = (await import("@whiskeysockets/baileys")).default
+import axios from 'axios';
+const { proto, generateWAMessageFromContent, prepareWAMessageMedia, generateWAMessageContent, getDevice } = (await import("@whiskeysockets/baileys")).default;
 
 let handler = async (message, { conn, text, usedPrefix, command }) => {
-    if (!text) return conn.reply(message.chat, '*\`『 🧚🏼‍♂️اكتب اسم الفديو الي انت عيزو معا الامر 』\`*', message)
+    if (!text) return conn.reply(message.chat, '*\`『 🧚🏼‍♂️اكتب اسم الفديو الي انت عيزو معا الامر 』\`*', message);
+
     async function createVideoMessage(url) {
-        const { videoMessage } = await generateWAMessageContent({ video: { url } }, { upload: conn.waUploadToServer })
-        return videoMessage
+        const { videoMessage } = await generateWAMessageContent({ video: { url } }, { upload: conn.waUploadToServer });
+        return videoMessage;
     }
+
     async function shuffleArray(array) {
         for (let i = array.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]]
+            [array[i], array[j]] = [array[j], array[i]];
         }
     }
+
     try {
-        let results = []
-        let { data: response } = await axios.get('https://apis-starlights-team.koyeb.app/starlight/tiktoksearch?text=' + text)
-        let searchResults = response.data
-        shuffleArray(searchResults)
-        let selectedResults = searchResults.splice(0, 7)
+        let results = [];
+        let { data: response } = await axios.get('https://apis-starlights-team.koyeb.app/starlight/tiktoksearch?text=' + text);
+        let searchResults = response.data;
+        shuffleArray(searchResults);
+
+        let selectedResults = searchResults.splice(0, 7);
         for (let result of selectedResults) {
             results.push({
                 body: proto.Message.InteractiveMessage.Body.fromObject({ text: null }),
@@ -29,8 +33,9 @@ let handler = async (message, { conn, text, usedPrefix, command }) => {
                     videoMessage: await createVideoMessage(result.nowm)
                 }),
                 nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({ buttons: [] })
-            })
+            });
         }
+
         const responseMessage = generateWAMessageFromContent(message.chat, {
             viewOnceMessage: {
                 message: {
@@ -46,14 +51,16 @@ let handler = async (message, { conn, text, usedPrefix, command }) => {
                     })
                 }
             }
-        }, { quoted: message })
-        await conn.relayMessage(message.chat, responseMessage.message, { messageId: responseMessage.key.id })
+        }, { quoted: message });
+
+        await conn.relayMessage(message.chat, responseMessage.message, { messageId: responseMessage.key.id });
     } catch (error) {
-        await conn.reply(message.chat, error.toString(), message)
+        await conn.reply(message.chat, error.toString(), message);
     }
 }
 
-handler.help = ['tiktoksearch <txt>']
-handler.tags = ['بحث']
-handler.command = ['ستوري', 'استوري', 'فيديو']
-export default handler
+handler.help = ['tiktoksearch <txt>'];
+handler.tags = ['بحث'];
+handler.command = ['ستوري', 'استوري', 'فيديو'];
+
+export default handler;
