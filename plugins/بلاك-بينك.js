25fd66245dfb3a12afb764012886043820ca4c83
safrot11 <safrot272@gmail.com>
2024-10-21 00:28:01 +0300

    Update بلاك-بينك.js

--- plugins/بلاك-بينك.js
+++ plugins/بلاك-بينك.js
@@ -8,7 +8,7 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
       const randomkpopx = randomkpop[Math.floor(Math.random() * randomkpop.length)]; // اختيار عشوائي من القائمة
 
       // إرسال الصورة مع الزر
-      conn.sendButton(m.chat, `_${command}_`, '𝐒𝐀𝐅𝐑𝐎𝐓-𝐁𝐎𝐓', randomkpopx, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m);
+      conn.sendButton(m.chat, `_${command}_`, '𝐒𝐀𝐅𝐑𝐎𝐓-𝐁𝐎𝐓', randomkpopx, [['🧚🏻‍♂️ صوره تاني', `/${command}`]], m);
     })
     .catch(err => console.error(err)); // معالجة الأخطاء في حالة حدوثها
 };
