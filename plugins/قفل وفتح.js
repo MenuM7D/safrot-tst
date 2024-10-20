4f7cc538c95d6a92d89d8ec8821468278223f6c3
safrot11 <safrot272@gmail.com>
2024-10-20 03:07:04 +0300

    Update قفل وفتح.js

--- plugins/قفل وفتح.js
+++ plugins/قفل وفتح.js
@@ -1,5 +1,5 @@
   let handler = async (m, { conn, args, usedPrefix, command }) => {
-  const pp ='https://telegra.ph/file/c73aac3f5ad3201fd4717.jpg'
+  const pp ='https://f.uguu.se/GPmXnURT.jpg'
   const fakecontact = { 'key': { 'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': '𝙎𝙖𝙛𝙧𝙤𝙩-𝘽𝙤𝙩' }, 'message': { 'contactMessage': { 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` } }, 'participant': '0@s.whatsapp.net' };
   let isClose = { // Switch Case Like :v
       'فتح': 'not_announcement',
@@ -9,7 +9,7 @@
       return conn.sendButton(
       m.chat,
       'اخـتـر', 
-      '𝘽𝙔: 𝙎𝙖𝙛𝙧𝙤𝙩 𝘽𝙊𝙏',
+      '> *\`『 𝐒𝐀𝐅𝐑𝐎𝐓-𝐁𝐎𝐓 』\`*',
       pp,
       [
         ['فتح', `${usedPrefix + command} فتح`],
