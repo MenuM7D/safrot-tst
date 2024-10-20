9bba75323b6f0b27e44fe2cd6274b29185feceac
safrot11 <safrot272@gmail.com>
2024-10-20 20:49:02 +0300

    Update اختبار_السرعه.js

--- plugins/اختبار_السرعه.js
+++ plugins/اختبار_السرعه.js
@@ -5,28 +5,28 @@ const exec = promisify(cp.exec).bind(cp);
 
 var handler = async (m, { conn }) => {
     let o;
-    await conn.reply(m.chat, global.wait, m);
+    await conn.reply(m.chat, global.wait, m); // بيرد على المستخدم وبيقوله استنى
     try {
-        o = await exec('python3 speed.py --secure --share');
+        o = await exec('python3 speed.py --secure --share'); // بيشغل الأمر الخاص بقياس السرعة
         const { stdout, stderr } = o;
         if (stdout.trim()) {
-            const match = stdout.match(/http[^"]+\.png/);
+            const match = stdout.match(/http[^"]+\.png/); // بيشوف إذا في رابط صورة
             const urlImagen = match ? match[0] : null;
-            await conn.sendMessage(m.chat, { image: { url: urlImagen }, caption: stdout.trim() }, { quoted: m });
+            await conn.sendMessage(m.chat, { image: { url: urlImagen }, caption: stdout.trim() }, { quoted: m }); // بيبعت النتيجة وصورة السرعة لو لقاها
         }
         if (stderr.trim()) {
-            const match2 = stderr.match(/http[^"]+\.png/);
+            const match2 = stderr.match(/http[^"]+\.png/); // نفس الموضوع لو في خطأ
             const urlImagen2 = match2 ? match2[0] : null;
             await conn.sendMessage(m.chat, { image: { url: urlImagen2 }, caption: stderr.trim() }, { quoted: m });
         }
     } catch (e) {
         o = e.message;
-        return m.reply(o);
+        return m.reply(o); // بيرد بالرسالة لو حصل خطأ
         console.log(e);
     }
 }
-handler.help = ["speedtest"];
-handler.tags = ["main"];
-handler.command = /^(اختبار_السرعه|test?speed)$/i;
+handler.help = ["اختبارسرعة"]; // المساعدة للكود
+handler.tags = ["رئيسي"]; // العلامات
+handler.command = /^(اختبار|اختبارسرعة)$/i; // الأمر المستخدم في الشات
 
 export default handler;
