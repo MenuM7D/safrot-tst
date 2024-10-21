ec1d6fbf9fc25cc9a85b6010b965949a05f5c431
safrot11 <safrot272@gmail.com>
2024-10-21 11:52:18 +0300

    Update اضافهxp.js

--- plugins/اضافهxp.js
+++ plugins/اضافهxp.js
@@ -1,29 +1,30 @@
-//import db from './database.js'
-
 let handler = async (m, { conn, text }) => {
   let who;
   if (m.isGroup) who = m.mentionedJid[0];
   else who = m.chat;
 
   if (!who) throw '✳️ *ضع علامة على المستخدم*';
-  
+
   let txt = text.replace('@' + who.split`@`[0], '').trim();
   if (!txt) throw '✳️ *أدخل مبلغ *خبرة* الذي تريد إضافته*';
   if (isNaN(txt)) throw ' 🔢 *أرقام فقط*';
-  
+
   let xp = parseInt(txt);
   let exp = xp;
-  
+
   if (exp < 1) throw '✳️ الحد الأدنى *1*';
-  
+
   let users = global.db.data.users;
+
+  // تحقق من وجود المستخدم في قاعدة البيانات
+  if (!users[who]) throw '✳️ *المستخدم غير موجود في قاعدة البيانات*';
+
   users[who].exp += xp;
 
   await m.reply(`≡ ~*تمت إضافة الخبرة*~
 *┓✥━═━═━━═━═━✥*
 *▢  ▢ المجموع: +${xp}*
 *┛✥━═━═━━═━═━✥*`);
-  // conn.fakeReply(m.chat, `▢ *لقد استقبلت* \n\n *+${xp} خبرة*`, who, m.text);
 }
 
 handler.help = ['addxp <@user>'];
