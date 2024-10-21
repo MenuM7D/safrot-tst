ffda1c1fa69e910e880d9151ee066deae4a221e7
safrot11 <safrot272@gmail.com>
2024-10-21 10:47:57 +0300

    Update سرعه.js

--- plugins/سرعه.js
+++ plugins/سرعه.js
@@ -1,21 +1,35 @@
 import cp from 'child_process';
-import {promisify} from 'util';
+import { promisify } from 'util';
 const exec = promisify(cp.exec).bind(cp);
 const handler = async (m) => {
-  await conn.reply(m.chat, global.wait, m);
+  await conn.reply(m.chat, '> *\`『 ويت🧚🏻‍♂️ 』\`*', m);
   let o;
   try {
     o = await exec('python3 speed.py');
   } catch (e) {
     o = e;
   } finally {
-    const {stdout, stderr} = o;
-    if (stdout.trim()) m.reply(stdout);
+    const { stdout, stderr } = o;
+    if (stdout.trim()) {
+      const arabicOutput = stdout
+        .replace(/< INFO - SPEEDTEST \/>/, '> *\`『 معلومات الاخطبار 』\`*')
+        .replace(/Iniciando prueba.../, '> *\`『 بداء الاخطبار 』\`*')
+        .replace(/Buscando servidor.../, '> *\`『 البحث عن الخادم 』\`*')
+        .replace(/Se selecionó el mejor servidor.../, '> *\`『 تم الخطبار 』\`*')
+        .replace(/ISP:/, '> *\`『 مزود الخدمه 』\`*')
+        .replace(/Servidor:/, '> *\`『 الخادم 』\`*')
+        .replace(/Ubicación:/, '> *\`『 الموقع 』\`*')
+        .replace(/Latencia:/, '> *\`『 الزمن المستغرق 』\`*')
+        .replace(/Descarga:/, '> *\`『 سرعة التنزيل 』\`*')
+        .replace(/Subida:/, '>*\`『 سرعة الرفع 』\`*');
+        
+      m.reply(arabicOutput);
+    }
     if (stderr.trim()) m.reply(stderr);
   }
 };
-handler.help = ['speedtest'];
-handler.tags = ['info'];
+handler.help = ['سرعه'];
+handler.tags = ['معلومات'];
 handler.command = /^(speedtest?|test?speed)|سرعه$/i;
 
 export default handler;
